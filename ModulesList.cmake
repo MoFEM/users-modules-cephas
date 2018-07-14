da5a51a63bcc0aed17148eb9fd2f7bc0b06c4fcb
Lukasz Kaczmarczyk <likask@wp.pl>
2018-07-14 19:43:38 +0100

    simplify code

--- ModulesList.cmake
+++ ModulesList.cmake
@@ -15,104 +15,13 @@
 set(UM_LIB_SOURCES "")
 
 # Those are default sub-modules
-# FIXME: This has to find solution as a install with sub-modules
 include(${UM_SOURCE_DIR}/basic_finite_elements/AddModule.cmake)
 
 # Users modules library, common for all programs
 add_library(users_modules ${UM_LIB_SOURCES})
 
-# Those modules could be downloaded with MoFEM.
-# FIXME: Do not delete this, it is  used with code testing. That will be renoved
-# for testing with docker container.
-if(WITH_MODULE_OBSOLETE)
-  if(NOT EXISTS ${UM_SOURCE_DIR}/obsolete)
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} clone https://bitbucket.org/likask/mofem_um_obsolete.git obsolete
-      WORKING_DIRECTORY ${UM_SOURCE_DIR}
-    )
-  endif(NOT EXISTS ${UM_SOURCE_DIR}/obsolete)
-endif(WITH_MODULE_OBSOLETE)
-
-if(WITH_MODULE_HOMOGENISATION)
-  if(NOT EXISTS ${UM_SOURCE_DIR}/homogenisation)
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} clone https://bitbucket.org/likask/mofem_um_homogenisation homogenisation
-      WORKING_DIRECTORY ${UM_SOURCE_DIR}
-    )
-  endif(NOT EXISTS ${UM_SOURCE_DIR}/homogenisation)
-endif(WITH_MODULE_HOMOGENISATION)
-
-if(WITH_MODULE_BONE_REMODELLING)
-  if(NOT EXISTS ${UM_SOURCE_DIR}/bone_remodelling)
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} clone https://bitbucket.org/likask/mofem_um_bone_remodelling.git bone_remodelling
-      WORKING_DIRECTORY ${UM_SOURCE_DIR}
-    )
-  endif(NOT EXISTS ${UM_SOURCE_DIR}/bone_remodelling)
-endif(WITH_MODULE_BONE_REMODELLING)
-
- if(WITH_MODULE_MWLS_APPROX)
-  if(NOT EXISTS ${UM_SOURCE_DIR}/mwls_approx)
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} clone git clone https://bitbucket.org/karol41/mofem_um_mwls.git mwls_approx
-      WORKING_DIRECTORY ${UM_SOURCE_DIR}
-    )
-  endif(NOT EXISTS ${UM_SOURCE_DIR}/mwls_approx)
-endif(WITH_MODULE_MWLS_APPROX)
-
-if(WITH_MODULE_FRACTURE_MECHANICS)
-  if(NOT EXISTS ${UM_SOURCE_DIR}/fracture_mechanics)
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} clone https://bitbucket.org/likask/mofem_um_fracture_mechanics fracture_mechanics
-      WORKING_DIRECTORY ${UM_SOURCE_DIR}
-    )
-  endif(NOT EXISTS ${UM_SOURCE_DIR}/fracture_mechanics)
-endif(WITH_MODULE_FRACTURE_MECHANICS)
-
-if(WITH_MODULE_GELS)
-  if(NOT EXISTS ${UM_SOURCE_DIR}/gels)
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} clone https://bitbucket.org/likask/mofem_um_gels gels
-      WORKING_DIRECTORY ${UM_SOURCE_DIR}
-    )
-  endif(NOT EXISTS ${UM_SOURCE_DIR}/gels)
-endif(WITH_MODULE_GELS)
-
-if(WITH_MODULE_STRAIN_PLASTICITY)
-  if(NOT EXISTS ${UM_SOURCE_DIR}/small_strain_plasticity)
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} clone https://bitbucket.org/likask/mofem_um_small_strain_plasticity small_strain_plasticity
-      WORKING_DIRECTORY ${UM_SOURCE_DIR}
-    )
-  endif(NOT EXISTS ${UM_SOURCE_DIR}/small_strain_plasticity)
-endif(WITH_MODULE_STRAIN_PLASTICITY)
-
-if(WITH_MODULE_SOLID_SHELL_PRISM_ELEMENT)
-  if(NOT EXISTS ${UM_SOURCE_DIR}/solid_shell_prism_element)
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} clone https://bitbucket.org/likask/mofem_um_solid_shell_prism_element solid_shell_prism_element
-      WORKING_DIRECTORY ${UM_SOURCE_DIR}
-    )
-  endif(NOT EXISTS ${UM_SOURCE_DIR}/solid_shell_prism_element)
-endif(WITH_MODULE_SOLID_SHELL_PRISM_ELEMENT)
-
-if(WITH_MODULE_MINIMAL_SURFACE_EQUATION)
-  if(NOT EXISTS ${UM_SOURCE_DIR}/minimal_surface_equation)
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} clone https://bitbucket.org/likask/mofem_um_minimal_surface_equation minimal_surface_equation
-      WORKING_DIRECTORY ${UM_SOURCE_DIR}
-    )
-  endif(NOT EXISTS ${UM_SOURCE_DIR}/minimal_surface_equation)
-endif(WITH_MODULE_MINIMAL_SURFACE_EQUATION)
-
-if(WITH_MODULE_HELMHOLTZ)
-  if(NOT EXISTS ${UM_SOURCE_DIR}/helmholtz)
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} clone https://bitbucket.org/likask/mofem_um_helmholtz helmholtz
-      WORKING_DIRECTORY ${UM_SOURCE_DIR}
-    )
-  endif(NOT EXISTS ${UM_SOURCE_DIR}/helmholtz)
-endif(WITH_MODULE_HELMHOLTZ)
+# Download some known modules (Obsolete)
+include(WithModules.cmake)
 
 file(
   GLOB_RECURSE INSTLLED_MODULES
@@ -137,20 +46,6 @@ add_custom_target(
   checkout_master
   COMMENT "Checkout master branch ..." VERBATIM
 )
-# add_custom_target(
-#   merge_CDashTesting
-#   COMMENT "Make merge CDashTesting branch ..." VERBATIM
-# )
-add_custom_target(
-  get_modules_contributors_init
-  COMMAND echo "Modules contributors" > ${PROJECT_SOURCE_DIR}/doc/contributors_list_modules
-  COMMENT "Init file for modules contributors list ..." VERBATIM
-)
-add_custom_target(
-  get_modules_contributors
-  COMMENT "Get module contributors ..." VERBATIM
-)
-add_dependencies(get_modules_contributors get_modules_contributors_init)
 
 # Recognise that module is installed
 foreach(LOOP_MODULE ${INSTLLED_MODULES})
@@ -208,24 +103,6 @@ foreach(LOOP_MODULE ${INSTLLED_MODULES})
     COMMENT "Checkout master baranch for module ${MODULE_NAME}" VERBATIM
   )
   add_dependencies(checkout_master checkout_master_${MODULE_NAME})
-  # add_custom_target(
-  #   merge_CDashTesting_${MODULE_NAME}
-  #   COMMAND ${GIT_EXECUTABLE} merge --ff CDashTesting
-  #   WORKING_DIRECTORY ${MODULE_DIRECTORY}
-  #   COMMENT "Make merge CDashTesting branch for module ${MODULE_NAME}" VERBATIM
-  # )
-  # add_dependencies(merge_CDashTesting merge_CDashTesting_${MODULE_NAME})
-  # get contributors
-  add_custom_target(
-    get_module_contributors_${MODULE_NAME}
-    COMMAND echo >> ${PROJECT_SOURCE_DIR}/doc/contributors_list_modules
-    COMMAND echo ${MODULE_NAME} >> ${PROJECT_SOURCE_DIR}/doc/contributors_list_modules
-    COMMAND ${GIT_EXECUTABLE} shortlog -s -e -n | sed "s/\@/ at /g" >> ${PROJECT_SOURCE_DIR}/doc/contributors_list_modules
-    WORKING_DIRECTORY ${MODULE_DIRECTORY}
-    COMMENT "Get module contributors ${MODULE_NAME}" VERBATIM
-  )
-  add_dependencies(get_module_contributors_${MODULE_NAME} get_modules_contributors_init)
-  add_dependencies(get_modules_contributors get_module_contributors_${MODULE_NAME})
   # include module
   include(${LOOP_MODULE})
 endforeach(LOOP_MODULE)
