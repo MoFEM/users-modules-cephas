d18f43f884bfbfbce50f0f70155d911a2d361164
Lukasz Kaczmarczyk <likask@wp.pl>
2018-08-29 16:43:10 +0100

    format code and error handling

--- basic_finite_elements/mix_transport/unsaturated_transport.cpp
+++ basic_finite_elements/mix_transport/unsaturated_transport.cpp
@@ -29,7 +29,7 @@ namespace po = boost::program_options;
 #include <MaterialUnsaturatedFlow.hpp>
 
 #ifndef WITH_ADOL_C
-  #error "MoFEM need to be compiled with ADOL-C"
+#error "MoFEM need to be compiled with ADOL-C"
 #endif
 
 using namespace MoFEM;
@@ -39,38 +39,38 @@ static char help[] = "...\n\n";
 double GenericMaterial::ePsilon0 = 0;
 double GenericMaterial::ePsilon1 = 0;
 
-map<std::string,CommonMaterialData::RegisterHook> RegisterMaterials::mapOfRegistredMaterials;
+map<std::string, CommonMaterialData::RegisterHook>
+    RegisterMaterials::mapOfRegistredMaterials;
 
 int main(int argc, char *argv[]) {
 
-  PetscInitialize(&argc,&argv,(char *)0,help);
+  MoFEM::Core::Initialize(&argc, &argv, (char *)0, help);
 
   // Register DM Manager
-  ierr = DMRegister_MoFEM("DMMOFEM"); CHKERRG(ierr); // register MoFEM DM in PETSc
+  CHKERR DMRegister_MoFEM("DMMOFEM"); // register MoFEM DM in PETSc
   // Register materials
-  ierr = RegisterMaterials()(); CHKERRG(ierr);
+  CHKERR RegisterMaterials()();
 
   PetscBool test_mat = PETSC_FALSE;
-  ierr = PetscOptionsGetBool(
-    PETSC_NULL,"","-test_mat",&test_mat,PETSC_NULL
-  ); CHKERRG(ierr);
+  CHKERR PetscOptionsGetBool(PETSC_NULL, "", "-test_mat", &test_mat,
+                             PETSC_NULL);
 
-  if(test_mat==PETSC_TRUE) {
+  if (test_mat == PETSC_TRUE) {
     CommonMaterialData data;
     // Testing van_genuchten
     MaterialVanGenuchten van_genuchten(data);
-    van_genuchten.printMatParameters(-1,"testing");
-    van_genuchten.printTheta(-1e-16,-1e4,-1e-12,"hTheta");
-    van_genuchten.printKappa(-1e-16,-1,-1e-12,"hK");
-    van_genuchten.printC(-1e-16,-1,-1e-12,"hC");
-    ierr = PetscFinalize(); CHKERRG(ierr);
+    van_genuchten.printMatParameters(-1, "testing");
+    van_genuchten.printTheta(-1e-16, -1e4, -1e-12, "hTheta");
+    van_genuchten.printKappa(-1e-16, -1, -1e-12, "hK");
+    van_genuchten.printC(-1e-16, -1, -1e-12, "hC");
+    CHKERR PetscFinalize();
     return 0;
   }
 
   try {
 
     moab::Core mb_instance;
-    moab::Interface& moab = mb_instance;
+    moab::Interface &moab = mb_instance;
 
     // get file name form command line
     PetscBool flg_mesh_file_name = PETSC_TRUE;
@@ -80,176 +80,191 @@ int main(int argc, char *argv[]) {
 
     int order = 1;
 
-    ierr = PetscOptionsBegin(
-      PETSC_COMM_WORLD,"",
-      "Unsaturated flow options","none"
-    ); CHKERRG(ierr);
+    ierr = PetscOptionsBegin(PETSC_COMM_WORLD, "", "Unsaturated flow options",
+                             "none");
+    CHKERRG(ierr);
+    ierr = PetscOptionsString("-my_file", "mesh file name", "", "mesh.h5m",
+                              mesh_file_name, 255, &flg_mesh_file_name);
+    CHKERRG(ierr);
     ierr = PetscOptionsString(
-      "-my_file",
-      "mesh file name","","mesh.h5m",mesh_file_name,255,&flg_mesh_file_name
-    ); CHKERRG(ierr);
-    ierr = PetscOptionsString(
-      "-configure",
-      "material and bc configuration file name","", "unsaturated.cfg",
-      conf_file_name,255,&flg_conf_file_name
-    ); CHKERRG(ierr);
-    ierr = PetscOptionsInt(
-      "-my_order",
-      "default approximation order","",
-      order,&order,PETSC_NULL
-    ); CHKERRG(ierr);
-    ierr = PetscOptionsEnd(); CHKERRG(ierr);
+        "-configure", "material and bc configuration file name", "",
+        "unsaturated.cfg", conf_file_name, 255, &flg_conf_file_name);
+    CHKERRG(ierr);
+    ierr = PetscOptionsInt("-my_order", "default approximation order", "",
+                           order, &order, PETSC_NULL);
+    CHKERRG(ierr);
+    ierr = PetscOptionsEnd();
+    CHKERRG(ierr);
 
-    if(flg_mesh_file_name != PETSC_TRUE) {
-      SETERRQ(PETSC_COMM_SELF,MOFEM_INVALID_DATA,"File name not set (e.g. -my_name mesh.h5m)");
+    if (flg_mesh_file_name != PETSC_TRUE) {
+      SETERRQ(PETSC_COMM_SELF, MOFEM_INVALID_DATA,
+              "File name not set (e.g. -my_name mesh.h5m)");
     }
-    if(flg_conf_file_name != PETSC_TRUE) {
-      SETERRQ(PETSC_COMM_SELF,MOFEM_INVALID_DATA,"File name not set (e.g. -config unsaturated.cfg)");
+    if (flg_conf_file_name != PETSC_TRUE) {
+      SETERRQ(PETSC_COMM_SELF, MOFEM_INVALID_DATA,
+              "File name not set (e.g. -config unsaturated.cfg)");
     }
 
     // Create MOAB communicator
     MPI_Comm moab_comm_world;
-    MPI_Comm_dup(PETSC_COMM_WORLD,&moab_comm_world);
-    ParallelComm* pcomm = ParallelComm::get_pcomm(&moab,MYPCOMM_INDEX);
-    if(pcomm == NULL) pcomm =  new ParallelComm(&moab,moab_comm_world);
+    MPI_Comm_dup(PETSC_COMM_WORLD, &moab_comm_world);
+    ParallelComm *pcomm = ParallelComm::get_pcomm(&moab, MYPCOMM_INDEX);
+    if (pcomm == NULL)
+      pcomm = new ParallelComm(&moab, moab_comm_world);
 
     const char *option;
     option = "PARALLEL=READ_PART;"
-    "PARALLEL_RESOLVE_SHARED_ENTS;"
-    "PARTITION=PARALLEL_PARTITION;";
-    rval = moab.load_file(mesh_file_name, 0, option); CHKERRG(rval);
+             "PARALLEL_RESOLVE_SHARED_ENTS;"
+             "PARTITION=PARALLEL_PARTITION;";
+    CHKERR moab.load_file(mesh_file_name, 0, option);
 
     // Create mofem interface
     MoFEM::Core core(moab);
-    MoFEM::Interface& m_field = core;
+    MoFEM::Interface &m_field = core;
 
     // Add meshsets with material and boundary conditions
     MeshsetsManager *meshsets_manager_ptr;
-    ierr = m_field.getInterface(meshsets_manager_ptr); CHKERRG(ierr);
-    ierr = meshsets_manager_ptr->setMeshsetFromFile(); CHKERRG(ierr);
+    CHKERR m_field.getInterface(meshsets_manager_ptr);
+    CHKERR meshsets_manager_ptr->setMeshsetFromFile();
 
-    PetscPrintf(PETSC_COMM_WORLD,"Read meshsets and added meshsets from bc.cfg\n");
-    for(_IT_CUBITMESHSETS_FOR_LOOP_(m_field,it)) {
-      PetscPrintf(
-        PETSC_COMM_WORLD,
-        "%s",static_cast<std::ostringstream&>(std::ostringstream().seekp(0) << *it << endl).str().c_str()
-      );
+    PetscPrintf(PETSC_COMM_WORLD,
+                "Read meshsets and added meshsets from bc.cfg\n");
+    for (_IT_CUBITMESHSETS_FOR_LOOP_(m_field, it)) {
+      PetscPrintf(PETSC_COMM_WORLD, "%s",
+                  static_cast<std::ostringstream &>(
+                      std::ostringstream().seekp(0) << *it << endl)
+                      .str()
+                      .c_str());
     }
 
     // Set entities bit level
-    ierr = m_field.getInterface<BitRefManager>()->setBitRefLevelByDim(0,3,BitRefLevel().set(0)); CHKERRG(ierr);
+    CHKERR m_field.getInterface<BitRefManager>()->setBitRefLevelByDim(
+        0, 3, BitRefLevel().set(0));
 
     UnsaturatedFlowElement uf(m_field);
 
     ifstream ini_file(conf_file_name);
     po::variables_map vm;
     po::options_description config_file_options;
-    Range domain_ents,bc_boundary_ents;
+    Range domain_ents, bc_boundary_ents;
 
-    map<int,CommonMaterialData> material_blocks;
-    map<int,double> head_blocks;
-    map<int,double> flux_blocks;
+    map<int, CommonMaterialData> material_blocks;
+    map<int, double> head_blocks;
+    map<int, double> flux_blocks;
     // Set material blocks
-    for(_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field,BLOCKSET,it)) {
-      if(it->getName().compare(0,4,"SOIL")!=0) continue;
+    for (_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field, BLOCKSET, it)) {
+      if (it->getName().compare(0, 4, "SOIL") != 0)
+        continue;
       // get block id
       const int block_id = it->getMeshsetId();
-      std::string block_name = "mat_block_"+boost::lexical_cast<std::string>(block_id);
+      std::string block_name =
+          "mat_block_" + boost::lexical_cast<std::string>(block_id);
       material_blocks[block_id].blockId = block_id;
-      material_blocks[block_id].addOptions(config_file_options,block_name);
+      material_blocks[block_id].addOptions(config_file_options, block_name);
     }
-    for(_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field,BLOCKSET,it)) {
-      if(it->getName().compare(0,4,"HEAD")!=0) continue;
+    for (_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field, BLOCKSET, it)) {
+      if (it->getName().compare(0, 4, "HEAD") != 0)
+        continue;
       // get block id
       const int block_id = it->getMeshsetId();
-      std::string block_name = "head_block_"+boost::lexical_cast<std::string>(block_id);
-      config_file_options.add_options()
-      ((block_name+".head").c_str(),po::value<double>(&head_blocks[block_id])->default_value(0));
+      std::string block_name =
+          "head_block_" + boost::lexical_cast<std::string>(block_id);
+      config_file_options.add_options()(
+          (block_name + ".head").c_str(),
+          po::value<double>(&head_blocks[block_id])->default_value(0));
     }
-    for(_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field,BLOCKSET,it)) {
-      if(it->getName().compare(0,4,"FLUX")!=0) continue;
+    for (_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field, BLOCKSET, it)) {
+      if (it->getName().compare(0, 4, "FLUX") != 0)
+        continue;
       // get block id
       const int block_id = it->getMeshsetId();
-      std::string block_name = "flux_block_"+boost::lexical_cast<std::string>(block_id);
-      config_file_options.add_options()
-      ((block_name+".flux").c_str(),po::value<double>(&head_blocks[block_id])->default_value(0));
+      std::string block_name =
+          "flux_block_" + boost::lexical_cast<std::string>(block_id);
+      config_file_options.add_options()(
+          (block_name + ".flux").c_str(),
+          po::value<double>(&head_blocks[block_id])->default_value(0));
     }
-    po::parsed_options parsed = parse_config_file(ini_file,config_file_options,true);
-    store(parsed,vm);
+    po::parsed_options parsed =
+        parse_config_file(ini_file, config_file_options, true);
+    store(parsed, vm);
     po::notify(vm);
-    for(_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field,BLOCKSET,it)) {
-      if(it->getName().compare(0,4,"SOIL")!=0) continue;
+    for (_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field, BLOCKSET, it)) {
+      if (it->getName().compare(0, 4, "SOIL") != 0)
+        continue;
       const int block_id = it->getMeshsetId();
-      uf.dMatMap[block_id] =
-      RegisterMaterials::mapOfRegistredMaterials.
-      at(material_blocks[block_id].matName)(material_blocks.at(block_id));
-      if(!uf.dMatMap.at(block_id)) {
-        SETERRQ(PETSC_COMM_WORLD,MOFEM_DATA_INCONSISTENCY,"Material block not set");
+      uf.dMatMap[block_id] = RegisterMaterials::mapOfRegistredMaterials.at(
+          material_blocks[block_id].matName)(material_blocks.at(block_id));
+      if (!uf.dMatMap.at(block_id)) {
+        SETERRQ(PETSC_COMM_WORLD, MOFEM_DATA_INCONSISTENCY,
+                "Material block not set");
       }
       // get block test
-      rval = m_field.get_moab().get_entities_by_type(
-        it->meshset,MBTET,uf.dMatMap.at(block_id)->tEts,true
-      ); CHKERRG(rval);
+      CHKERR m_field.get_moab().get_entities_by_type(
+          it->meshset, MBTET, uf.dMatMap.at(block_id)->tEts, true);
       domain_ents.merge(uf.dMatMap.at(block_id)->tEts);
-      uf.dMatMap.at(block_id)->printMatParameters(block_id,"Read material");
+      uf.dMatMap.at(block_id)->printMatParameters(block_id, "Read material");
     }
     std::vector<std::string> additional_parameters;
-    additional_parameters = collect_unrecognized(parsed.options,po::include_positional);
-    for(std::vector<std::string>::iterator vit = additional_parameters.begin();
-    vit!=additional_parameters.end();vit++) {
-      ierr = PetscPrintf(m_field.get_comm(),"** WARNING Unrecognized option %s\n",vit->c_str()); CHKERRG(ierr);
+    additional_parameters =
+        collect_unrecognized(parsed.options, po::include_positional);
+    for (std::vector<std::string>::iterator vit = additional_parameters.begin();
+         vit != additional_parameters.end(); vit++) {
+      CHKERR PetscPrintf(m_field.get_comm(),
+                         "** WARNING Unrecognized option %s\n", vit->c_str());
     }
     // Set capillary pressure bc data
-    for(_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field,BLOCKSET,it)) {
-      if(it->getName().compare(0,4,"HEAD")!=0) continue;
+    for (_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field, BLOCKSET, it)) {
+      if (it->getName().compare(0, 4, "HEAD") != 0)
+        continue;
       // get block id
       const int block_id = it->getMeshsetId();
       // create block data instance
-      uf.bcValueMap[block_id] = boost::shared_ptr<UnsaturatedFlowElement::BcData>(
-        new UnsaturatedFlowElement::BcData()
-      );
+      uf.bcValueMap[block_id] =
+          boost::shared_ptr<UnsaturatedFlowElement::BcData>(
+              new UnsaturatedFlowElement::BcData());
       // get bc value
       std::vector<double> attributes;
-      ierr = it->getAttributes(attributes); CHKERRG(ierr);
+      CHKERR it->getAttributes(attributes);
       uf.bcValueMap[block_id]->fixValue = attributes[0];
-      std::string block_name = "head_block_"+boost::lexical_cast<std::string>(block_id);
-      if(vm.count((block_name)+".head")) {
+      std::string block_name =
+          "head_block_" + boost::lexical_cast<std::string>(block_id);
+      if (vm.count((block_name) + ".head")) {
         uf.bcValueMap[block_id]->fixValue = head_blocks[block_id];
       }
       // cerr << uf.bcValueMap[block_id]->fixValue  << endl;
-      // ierr = it->printAttributes(std::cout); CHKERRG(ierr);
+      // CHKERR it->printAttributes(std::cout);
       // get faces in the block
-      rval = m_field.get_moab().get_entities_by_type(
-        it->meshset,MBTRI,uf.bcValueMap[block_id]->eNts,true
-      ); CHKERRG(rval);
+      CHKERR m_field.get_moab().get_entities_by_type(
+          it->meshset, MBTRI, uf.bcValueMap[block_id]->eNts, true);
       bc_boundary_ents.merge(uf.bcValueMap[block_id]->eNts);
     }
 
     int max_flux_id = 0;
     // Set water flux bc data
-    for(_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field,BLOCKSET,it)) {
-      if(it->getName().compare(0,4,"FLUX")!=0) continue;
+    for (_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field, BLOCKSET, it)) {
+      if (it->getName().compare(0, 4, "FLUX") != 0)
+        continue;
       // get block id
       const int block_id = it->getMeshsetId();
       // create block data instance
-      uf.bcFluxMap[block_id] = boost::shared_ptr<UnsaturatedFlowElement::BcData>(
-        new UnsaturatedFlowElement::BcData()
-      );
+      uf.bcFluxMap[block_id] =
+          boost::shared_ptr<UnsaturatedFlowElement::BcData>(
+              new UnsaturatedFlowElement::BcData());
       // get bc value
       std::vector<double> attributes;
-      ierr = it->getAttributes(attributes); CHKERRG(ierr);
-      // ierr = it->printAttributes(std::cout); CHKERRG(ierr);
+      CHKERR it->getAttributes(attributes);
+      // CHKERR it->printAttributes(std::cout);
       uf.bcFluxMap[block_id]->fixValue = attributes[0];
-      std::string block_name = "flux_block_"+boost::lexical_cast<std::string>(block_id);
-      if(vm.count((block_name)+".flux")) {
+      std::string block_name =
+          "flux_block_" + boost::lexical_cast<std::string>(block_id);
+      if (vm.count((block_name) + ".flux")) {
         uf.bcValueMap[block_id]->fixValue = head_blocks[block_id];
       }
       // get faces in the block
-      rval = m_field.get_moab().get_entities_by_type(
-        it->meshset,MBTRI,uf.bcFluxMap[block_id]->eNts,true
-      ); CHKERRG(rval);
+      CHKERR m_field.get_moab().get_entities_by_type(
+          it->meshset, MBTRI, uf.bcFluxMap[block_id]->eNts, true);
       bc_boundary_ents.merge(uf.bcFluxMap[block_id]->eNts);
-      max_flux_id = max_flux_id>block_id ? max_flux_id : block_id+1;
+      max_flux_id = max_flux_id > block_id ? max_flux_id : block_id + 1;
     }
 
     // Add zero flux on the rest of the boundary
@@ -257,39 +272,36 @@ int main(int argc, char *argv[]) {
     {
       Skinner skin(&m_field.get_moab());
       Range domain_skin;
-      rval = skin.find_skin(0,domain_ents,false,domain_skin); CHKERRG(rval);
+      CHKERR skin.find_skin(0, domain_ents, false, domain_skin);
       // filter not owned entities, those are not on boundary
-      rval = pcomm->filter_pstatus(
-        domain_skin,PSTATUS_SHARED|PSTATUS_MULTISHARED,PSTATUS_NOT,-1,&zero_flux_ents
-      ); CHKERRG(rval);
-      zero_flux_ents = subtract(zero_flux_ents,bc_boundary_ents);
-      uf.bcFluxMap[max_flux_id] = boost::shared_ptr<UnsaturatedFlowElement::BcData>(
-        new UnsaturatedFlowElement::BcData()
-      );
+      CHKERR pcomm->filter_pstatus(domain_skin,
+                                   PSTATUS_SHARED | PSTATUS_MULTISHARED,
+                                   PSTATUS_NOT, -1, &zero_flux_ents);
+      zero_flux_ents = subtract(zero_flux_ents, bc_boundary_ents);
+      uf.bcFluxMap[max_flux_id] =
+          boost::shared_ptr<UnsaturatedFlowElement::BcData>(
+              new UnsaturatedFlowElement::BcData());
       uf.bcFluxMap[max_flux_id]->fixValue = 0;
       uf.bcFluxMap[max_flux_id]->eNts = zero_flux_ents;
     }
 
-    ierr = uf.addFields("VALUES","FLUXES",order); CHKERRG(ierr);
-    ierr = uf.addFiniteElements("FLUXES","VALUES"); CHKERRG(ierr);
-    ierr = m_field.add_ents_to_finite_element_by_type(
-      zero_flux_ents,MBTRI,"MIX_BCFLUX"
-    ); CHKERRG(ierr);
-    ierr = uf.buildProblem(); CHKERRG(ierr);
-    ierr = uf.createMatrices(); CHKERRG(ierr);
-    ierr = uf.setFiniteElements(); CHKERRG(ierr);
-    ierr = uf.calculateEssentialBc(); CHKERRG(ierr);
-    ierr = uf.calculateInitialPc(); CHKERRG(ierr);
-    ierr = uf.solveProblem(); CHKERRG(ierr);
-    ierr = uf.destroyMatrices(); CHKERRG(ierr);
+    CHKERR uf.addFields("VALUES", "FLUXES", order);
+    CHKERR uf.addFiniteElements("FLUXES", "VALUES");
+    CHKERR m_field.add_ents_to_finite_element_by_type(zero_flux_ents, MBTRI,
+                                                      "MIX_BCFLUX");
+    CHKERR uf.buildProblem();
+    CHKERR uf.createMatrices();
+    CHKERR uf.setFiniteElements();
+    CHKERR uf.calculateEssentialBc();
+    CHKERR uf.calculateInitialPc();
+    CHKERR uf.solveProblem();
+    CHKERR uf.destroyMatrices();
 
     MPI_Comm_free(&moab_comm_world);
-
-  } catch (MoFEMException const &e) {
-    SETERRQ(PETSC_COMM_SELF,e.errorCode,e.errorMessage);
   }
+  CATCH_ERRORS;
 
-  ierr = PetscFinalize(); CHKERRG(ierr);
+  CHKERR MoFEM::Core::Finalize();
 
   return 0;
 }
