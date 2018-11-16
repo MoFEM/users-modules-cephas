a14b6867e62d247e9d79a41204214416a5313c47
Lukasz Kaczmarczyk <likask@wp.pl>
2018-11-16 23:16:02 +0000

    add tool to add meshsets to file

---
+++ tools/add_meshsets.cpp
@@ -0,0 +1,74 @@
+/** \file add_meshsets.cpp
+
+  \brief Add meshsets
+
+*/
+
+/* This file is part of MoFEM.
+ * MoFEM is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * MoFEM is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with MoFEM. If not, see <http://www.gnu.org/licenses/>. */
+
+#include <MoFEM.hpp>
+using namespace MoFEM;
+
+static char help[] = "...\n\n";
+
+int main(int argc, char *argv[]) {
+  MoFEM::Core::Initialize(&argc, &argv, (char *)0, help);
+
+  try {
+
+    moab::Core mb_instance;
+    moab::Interface &moab = mb_instance;
+
+    char mesh_file_name[255];
+    PetscBool flg_file = PETSC_FALSE;
+    ierr = PetscOptionsBegin(PETSC_COMM_WORLD, "", "none", "none");
+    CHKERRQ(ierr);
+    CHKERR PetscOptionsString("-my_file", "mesh file name", "", "mesh.h5m",
+                              mesh_file_name, 255, &flg_file);
+    ierr = PetscOptionsEnd();
+    CHKERRQ(ierr);
+
+    const char *option;
+    option = "";
+    CHKERR moab.load_file(mesh_file_name, 0, option);
+
+    ParallelComm *pcomm = ParallelComm::get_pcomm(&moab, MYPCOMM_INDEX);
+    if (pcomm == NULL)
+      pcomm = new ParallelComm(&moab, PETSC_COMM_WORLD);
+
+    // Create MoFEM database
+    MoFEM::Core core(moab);
+    MoFEM::Interface &m_field = core;
+
+    MeshsetsManager *meshsets_interface_ptr;
+    CHKERR m_field.getInterface(meshsets_interface_ptr);
+    CHKERR meshsets_interface_ptr->setMeshsetFromFile();
+
+    std::cout << "Print all meshsets (old and added from meshsets "
+                 "configurational file\n"
+              << std::endl;
+    for (auto cit = meshsets_interface_ptr->getBegin();
+         cit != meshsets_interface_ptr->getEnd(); cit++) {
+      std::cout << *cit << endl;
+    }
+
+    CHKERR moab.write_file("out.h5m");
+  }
+  CATCH_ERRORS;
+
+  MoFEM::Core::Finalize();
+
+  return 0;
+}
