10f64c6d9c494d61e5b730f32a4283de56aa3440
Lukasz Kaczmarczyk <likask@wp.pl>
2019-01-04 10:37:44 +0000

    format code and error handling

--- basic_finite_elements/atom_tests/nonlinear_elastic.cpp
+++ basic_finite_elements/atom_tests/nonlinear_elastic.cpp
@@ -26,42 +26,41 @@
 using namespace MoFEM;
 
 namespace bio = boost::iostreams;
-using bio::tee_device;
 using bio::stream;
-
-
-
+using bio::tee_device;
 
 static char help[] = "...\n\n";
 
 int main(int argc, char *argv[]) {
 
-  MoFEM::Core::Initialize(&argc,&argv,(char *)0,help);
+  MoFEM::Core::Initialize(&argc, &argv, (char *)0, help);
 
   try {
 
     moab::Core mb_instance;
-    moab::Interface& moab = mb_instance;
+    moab::Interface &moab = mb_instance;
     int rank;
-    MPI_Comm_rank(PETSC_COMM_WORLD,&rank);
+    MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
 
     PetscBool flg = PETSC_TRUE;
     char mesh_file_name[255];
-    ierr = PetscOptionsGetString(PETSC_NULL,PETSC_NULL,"-my_file",mesh_file_name,255,&flg); CHKERRG(ierr);
-    if(flg != PETSC_TRUE) {
-      SETERRQ(PETSC_COMM_SELF,1,"*** ERROR -my_file (MESH FILE NEEDED)");
+    CHKERR PetscOptionsGetString(PETSC_NULL, PETSC_NULL, "-my_file",
+                                 mesh_file_name, 255, &flg);
+    if (flg != PETSC_TRUE) {
+      SETERRQ(PETSC_COMM_SELF, 1, "*** ERROR -my_file (MESH FILE NEEDED)");
     }
 
     const char *option;
-    option = "";//"PARALLEL=BCAST;";//;DEBUG_IO";
-    rval = moab.load_file(mesh_file_name, 0, option); CHKERRG(rval);
-    ParallelComm* pcomm = ParallelComm::get_pcomm(&moab,MYPCOMM_INDEX);
-    if(pcomm == NULL) pcomm =  new ParallelComm(&moab,PETSC_COMM_WORLD);
+    option = ""; //"PARALLEL=BCAST;";//;DEBUG_IO";
+    CHKERR moab.load_file(mesh_file_name, 0, option);
+    ParallelComm *pcomm = ParallelComm::get_pcomm(&moab, MYPCOMM_INDEX);
+    if (pcomm == NULL)
+      pcomm = new ParallelComm(&moab, PETSC_COMM_WORLD);
 
     MoFEM::Core core(moab);
-    MoFEM::Interface& m_field = core;
+    MoFEM::Interface &m_field = core;
 
-    //ref meshset ref level 0
+    // ref meshset ref level 0
     BitRefLevel bit_level0;
     bit_level0.set(0);
     EntityHandle meshset_level0;
@@ -71,29 +70,38 @@ int main(int argc, char *argv[]) {
     CHKERR m_field.getInterface<BitRefManager>()->getEntitiesByRefLevel(
         bit_level0, BitRefLevel().set(), meshset_level0);
 
-    //Fields
-    ierr = m_field.add_field("SPATIAL_POSITION",H1,AINSWORTH_LEGENDRE_BASE,3); CHKERRG(ierr);
-    //add entitities (by tets) to the field
-    ierr = m_field.add_ents_to_field_by_type(0,MBTET,"SPATIAL_POSITION"); CHKERRG(ierr);
-    //set app. order
+    // Fields
+    CHKERR m_field.add_field("SPATIAL_POSITION", H1, AINSWORTH_LEGENDRE_BASE,
+                             3);
+    // add entitities (by tets) to the field
+    CHKERR m_field.add_ents_to_field_by_type(0, MBTET, "SPATIAL_POSITION");
+    // set app. order
     PetscInt order;
-    ierr = PetscOptionsGetInt(PETSC_NULL,PETSC_NULL,"-my_order",&order,&flg); CHKERRG(ierr);
-    if(flg != PETSC_TRUE) {
+    CHKERR PetscOptionsGetInt(PETSC_NULL, PETSC_NULL, "-my_order", &order,
+                              &flg);
+    if (flg != PETSC_TRUE) {
       order = 1;
     }
-    ierr = m_field.set_field_order(0,MBTET,"SPATIAL_POSITION",order); CHKERRG(ierr);
-    ierr = m_field.set_field_order(0,MBTRI,"SPATIAL_POSITION",order); CHKERRG(ierr);
-    ierr = m_field.set_field_order(0,MBEDGE,"SPATIAL_POSITION",order); CHKERRG(ierr);
-    ierr = m_field.set_field_order(0,MBVERTEX,"SPATIAL_POSITION",1); CHKERRG(ierr);
+    CHKERR m_field.set_field_order(0, MBTET, "SPATIAL_POSITION", order);
+    CHKERR m_field.set_field_order(0, MBTRI, "SPATIAL_POSITION", order);
+    CHKERR m_field.set_field_order(0, MBEDGE, "SPATIAL_POSITION", order);
+    CHKERR m_field.set_field_order(0, MBVERTEX, "SPATIAL_POSITION", 1);
 
-    NonlinearElasticElement elastic(m_field,1);
-    boost::shared_ptr<NonlinearElasticElement::FunctionsToCalculatePiolaKirchhoffI<double> >
-    double_kirchhoff_material_ptr(new NonlinearElasticElement::FunctionsToCalculatePiolaKirchhoffI<double>());
-    boost::shared_ptr<NonlinearElasticElement::FunctionsToCalculatePiolaKirchhoffI<adouble> >
-    adouble_kirchhoff_material_ptr(new NonlinearElasticElement::FunctionsToCalculatePiolaKirchhoffI<adouble>());
-    ierr = elastic.setBlocks(double_kirchhoff_material_ptr,adouble_kirchhoff_material_ptr); CHKERRG(ierr);
-    ierr = elastic.addElement("ELASTIC","SPATIAL_POSITION"); CHKERRG(ierr);
-    ierr = elastic.setOperators("SPATIAL_POSITION"); CHKERRG(ierr);
+    NonlinearElasticElement elastic(m_field, 1);
+    boost::shared_ptr<
+        NonlinearElasticElement::FunctionsToCalculatePiolaKirchhoffI<double>>
+    double_kirchhoff_material_ptr(
+        new NonlinearElasticElement::FunctionsToCalculatePiolaKirchhoffI<
+            double>());
+    boost::shared_ptr<
+        NonlinearElasticElement::FunctionsToCalculatePiolaKirchhoffI<adouble>>
+        adouble_kirchhoff_material_ptr(
+            new NonlinearElasticElement::FunctionsToCalculatePiolaKirchhoffI<
+                adouble>());
+    CHKERR elastic.setBlocks(double_kirchhoff_material_ptr,
+                             adouble_kirchhoff_material_ptr);
+    CHKERR elastic.addElement("ELASTIC", "SPATIAL_POSITION");
+    CHKERR elastic.setOperators("SPATIAL_POSITION");
 
     /*struct MyMat: public FunctionsToCalculatePiolaKirchhoffI {
     Interface& moAB;
@@ -107,119 +115,122 @@ int main(int argc, char *argv[]) {
   }
 };
 MyMat mymat(moab);
-ierr = elastic.setOperators(mymat,"SPATIAL_POSITION"); CHKERRG(ierr);*/
+CHKERR elastic.setOperators(mymat,"SPATIAL_POSITION"); */
 
-//define problems
-ierr = m_field.add_problem("ELASTIC_MECHANICS"); CHKERRG(ierr);
-//set refinement level for problem
-ierr = m_field.modify_problem_ref_level_add_bit("ELASTIC_MECHANICS",bit_level0); CHKERRG(ierr);
-//set finite elements for problems
-ierr = m_field.modify_problem_add_finite_element("ELASTIC_MECHANICS","ELASTIC"); CHKERRG(ierr);
+    // define problems
+    CHKERR m_field.add_problem("ELASTIC_MECHANICS");
+    // set refinement level for problem
+    CHKERR m_field.modify_problem_ref_level_add_bit("ELASTIC_MECHANICS",
+                                                    bit_level0);
+    // set finite elements for problems
+    CHKERR m_field.modify_problem_add_finite_element("ELASTIC_MECHANICS",
+                                                     "ELASTIC");
 
-//build field
-ierr = m_field.build_fields(); CHKERRG(ierr);
+    // build field
+    CHKERR m_field.build_fields();
 
-//use this to apply some strain field to the body (testing only)
-double scale_positions = 2;
-{
-  EntityHandle node = 0;
-  double coords[3];
-  for(_IT_GET_DOFS_FIELD_BY_NAME_FOR_LOOP_(m_field,"SPATIAL_POSITION",dof_ptr)) {
-    if(dof_ptr->get()->getEntType()!=MBVERTEX) continue;
-    EntityHandle ent = dof_ptr->get()->getEnt();
-    int dof_rank = dof_ptr->get()->getDofCoeffIdx();
-    double &fval = dof_ptr->get()->getFieldData();
-    if(node!=ent) {
-      rval = moab.get_coords(&ent,1,coords); CHKERRG(rval);
-      node = ent;
+    // use this to apply some strain field to the body (testing only)
+    double scale_positions = 2;
+    {
+      EntityHandle node = 0;
+      double coords[3];
+      for (_IT_GET_DOFS_FIELD_BY_NAME_FOR_LOOP_(m_field, "SPATIAL_POSITION",
+                                                dof_ptr)) {
+        if (dof_ptr->get()->getEntType() != MBVERTEX)
+          continue;
+        EntityHandle ent = dof_ptr->get()->getEnt();
+        int dof_rank = dof_ptr->get()->getDofCoeffIdx();
+        double &fval = dof_ptr->get()->getFieldData();
+        if (node != ent) {
+          CHKERR moab.get_coords(&ent, 1, coords);
+          node = ent;
+        }
+        fval = scale_positions * coords[dof_rank];
+      }
     }
-    fval = scale_positions*coords[dof_rank];
+
+    // build finite elemnts
+    CHKERR m_field.build_finite_elements();
+    // build adjacencies
+    CHKERR m_field.build_adjacencies(bit_level0);
+
+    ProblemsManager *prb_mng_ptr;
+    CHKERR m_field.getInterface(prb_mng_ptr);
+    // build problem
+    CHKERR prb_mng_ptr->buildProblem("ELASTIC_MECHANICS", true);
+    // partition
+    CHKERR prb_mng_ptr->partitionProblem("ELASTIC_MECHANICS");
+    CHKERR prb_mng_ptr->partitionFiniteElements("ELASTIC_MECHANICS");
+    CHKERR prb_mng_ptr->partitionGhostDofs("ELASTIC_MECHANICS");
+
+    // create matrices
+    Vec F;
+    CHKERR m_field.getInterface<VecManager>()->vecCreateGhost(
+        "ELASTIC_MECHANICS", COL, &F);
+    // Vec D;
+    // CHKERR VecDuplicate(F,&D);
+    Mat Aij;
+    CHKERR m_field.MatCreateMPIAIJWithArrays("ELASTIC_MECHANICS", &Aij);
+
+    elastic.getLoopFeRhs().snes_f = F;
+    elastic.getLoopFeLhs().snes_B = Aij;
+
+    CHKERR m_field.loop_finite_elements("ELASTIC_MECHANICS", "ELASTIC",
+                                        elastic.getLoopFeRhs());
+    CHKERR VecGhostUpdateBegin(F, ADD_VALUES, SCATTER_REVERSE);
+    CHKERR VecGhostUpdateEnd(F, ADD_VALUES, SCATTER_REVERSE);
+    CHKERR VecAssemblyBegin(F);
+    CHKERR VecAssemblyEnd(F);
+
+    CHKERR m_field.loop_finite_elements("ELASTIC_MECHANICS", "ELASTIC",
+                                        elastic.getLoopFeLhs());
+    CHKERR MatAssemblyBegin(Aij, MAT_FINAL_ASSEMBLY);
+    CHKERR MatAssemblyEnd(Aij, MAT_FINAL_ASSEMBLY);
+
+    // PetscViewer viewer;
+    // CHKERR
+    // PetscViewerASCIIOpen(PETSC_COMM_WORLD,"nonlinear_elastic.txt",&viewer);
+
+    // CHKERR VecChop(F,1e-4);
+    // //CHKERR VecView(F,PETSC_VIEWER_STDOUT_WORLD);
+    // CHKERR VecView(F,viewer);
+    //
+    // //MatView(Aij,PETSC_VIEWER_DRAW_WORLD);
+    // MatChop(Aij,1e-4);
+    // //MatView(Aij,PETSC_VIEWER_STDOUT_WORLD);
+    // MatView(Aij,viewer);
+    // //std::string wait;
+    // //std::cin >> wait;
+    // CHKERR PetscViewerDestroy(&viewer);
+
+    double sum = 0;
+    CHKERR VecSum(F, &sum);
+    CHKERR PetscPrintf(PETSC_COMM_WORLD, "sum  = %4.3e\n", sum);
+    double fnorm;
+    CHKERR VecNorm(F, NORM_2, &fnorm);
+    CHKERR PetscPrintf(PETSC_COMM_WORLD, "fnorm  = %9.8e\n", fnorm);
+
+    double mnorm;
+    CHKERR MatNorm(Aij, NORM_1, &mnorm);
+    CHKERR PetscPrintf(PETSC_COMM_WORLD, "mnorm  = %9.8e\n", mnorm);
+
+    if (fabs(sum) > 1e-8) {
+      SETERRQ(PETSC_COMM_WORLD, MOFEM_ATOM_TEST_INVALID, "Failed to pass test");
+    }
+    if (fabs(fnorm - 5.12196914e+00) > 1e-6) {
+      SETERRQ(PETSC_COMM_WORLD, MOFEM_ATOM_TEST_INVALID, "Failed to pass test");
+    }
+    if (fabs(mnorm - 5.48280139e+01) > 1e-6) {
+      SETERRQ(PETSC_COMM_WORLD, MOFEM_ATOM_TEST_INVALID, "Failed to pass test");
+    }
+
+    CHKERR VecDestroy(&F);
+    // CHKERR VecDestroy(&D);
+    CHKERR MatDestroy(&Aij);
   }
-}
-
-//build finite elemnts
-ierr = m_field.build_finite_elements(); CHKERRG(ierr);
-//build adjacencies
-ierr = m_field.build_adjacencies(bit_level0); CHKERRG(ierr);
-
-
-ProblemsManager *prb_mng_ptr;
-ierr = m_field.getInterface(prb_mng_ptr); CHKERRG(ierr);
-//build problem
-ierr = prb_mng_ptr->buildProblem("ELASTIC_MECHANICS",true); CHKERRG(ierr);
-//partition
-ierr = prb_mng_ptr->partitionProblem("ELASTIC_MECHANICS"); CHKERRG(ierr);
-ierr = prb_mng_ptr->partitionFiniteElements("ELASTIC_MECHANICS"); CHKERRG(ierr);
-ierr = prb_mng_ptr->partitionGhostDofs("ELASTIC_MECHANICS"); CHKERRG(ierr);
-
-//create matrices
-Vec F;
-ierr = m_field.getInterface<VecManager>()->vecCreateGhost("ELASTIC_MECHANICS",COL,&F); CHKERRG(ierr);
-//Vec D;
-//ierr = VecDuplicate(F,&D); CHKERRG(ierr);
-Mat Aij;
-ierr = m_field.MatCreateMPIAIJWithArrays("ELASTIC_MECHANICS",&Aij); CHKERRG(ierr);
-
-elastic.getLoopFeRhs().snes_f = F;
-elastic.getLoopFeLhs().snes_B = Aij;
-
-ierr = m_field.loop_finite_elements("ELASTIC_MECHANICS","ELASTIC",elastic.getLoopFeRhs()); CHKERRG(ierr);
-ierr = VecGhostUpdateBegin(F,ADD_VALUES,SCATTER_REVERSE); CHKERRG(ierr);
-ierr = VecGhostUpdateEnd(F,ADD_VALUES,SCATTER_REVERSE); CHKERRG(ierr);
-ierr = VecAssemblyBegin(F); CHKERRG(ierr);
-ierr = VecAssemblyEnd(F); CHKERRG(ierr);
-
-ierr = m_field.loop_finite_elements("ELASTIC_MECHANICS","ELASTIC",elastic.getLoopFeLhs()); CHKERRG(ierr);
-ierr = MatAssemblyBegin(Aij,MAT_FINAL_ASSEMBLY); CHKERRG(ierr);
-ierr = MatAssemblyEnd(Aij,MAT_FINAL_ASSEMBLY); CHKERRG(ierr);
-
-// PetscViewer viewer;
-// ierr = PetscViewerASCIIOpen(PETSC_COMM_WORLD,"nonlinear_elastic.txt",&viewer); CHKERRG(ierr);
-
-// ierr = VecChop(F,1e-4); CHKERRG(ierr);
-// //ierr = VecView(F,PETSC_VIEWER_STDOUT_WORLD); CHKERRG(ierr);
-// ierr = VecView(F,viewer); CHKERRG(ierr);
-//
-// //MatView(Aij,PETSC_VIEWER_DRAW_WORLD);
-// MatChop(Aij,1e-4);
-// //MatView(Aij,PETSC_VIEWER_STDOUT_WORLD);
-// MatView(Aij,viewer);
-// //std::string wait;
-// //std::cin >> wait;
-// ierr = PetscViewerDestroy(&viewer); CHKERRG(ierr);
-
-double sum = 0;
-ierr = VecSum(F,&sum); CHKERRG(ierr);
-ierr = PetscPrintf(PETSC_COMM_WORLD,"sum  = %4.3e\n",sum); CHKERRG(ierr);
-double fnorm;
-ierr = VecNorm(F,NORM_2,&fnorm); CHKERRG(ierr);
-ierr = PetscPrintf(PETSC_COMM_WORLD,"fnorm  = %9.8e\n",fnorm); CHKERRG(ierr);
-
-double mnorm;
-ierr = MatNorm(Aij,NORM_1,&mnorm); CHKERRG(ierr);
-ierr = PetscPrintf(PETSC_COMM_WORLD,"mnorm  = %9.8e\n",mnorm); CHKERRG(ierr);
-
-
-if(fabs(sum)>1e-8) {
-  SETERRQ(PETSC_COMM_WORLD,MOFEM_ATOM_TEST_INVALID,"Failed to pass test");
-}
-if(fabs(fnorm-5.12196914e+00)>1e-6) {
-  SETERRQ(PETSC_COMM_WORLD,MOFEM_ATOM_TEST_INVALID,"Failed to pass test");
-}
-if(fabs(mnorm-5.48280139e+01)>1e-6) {
-  SETERRQ(PETSC_COMM_WORLD,MOFEM_ATOM_TEST_INVALID,"Failed to pass test");
-}
-
-
-ierr = VecDestroy(&F); CHKERRG(ierr);
-//ierr = VecDestroy(&D); CHKERRG(ierr);
-ierr = MatDestroy(&Aij); CHKERRG(ierr);
-
-} CATCH_ERRORS;
+  CATCH_ERRORS;
 
   MoFEM::Core::Finalize();
 
   return 0;
-
-
 }
