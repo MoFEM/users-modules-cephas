2990b023642448f25fcb932ef21f6b49c62d63d7
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2019-06-04 23:23:43 +0100

    refactor code

--- basic_finite_elements/level_set/level_set.cpp
+++ basic_finite_elements/level_set/level_set.cpp
@@ -15,67 +15,12 @@ using FaceEle = FaceElementForcesAndSourcesCore;
 using OpFaceEle = FaceElementForcesAndSourcesCore::UserDataOperator;
 using EntData = DataForcesAndSourcesCore::EntData;
 
-struct CalculateDistantFromSurface {
-
-  CalculateDistantFromSurface(MoFEM::Interface &m_field) : mField(m_field) {}
-
-  MoFEMErrorCode operator()(const Range &surface) {
-    MoFEMFunctionBegin;
-    treeSurfPtr = boost::shared_ptr<OrientedBoxTreeTool>(
-        new OrientedBoxTreeTool(&mField.get_moab(), "ROOTSETSURF", true));
-    CHKERR treeSurfPtr->build(surface, rootSetSurf);
-    DistanceFromVertex surf_dist(treeSurfPtr, rootSetSurf);
-    CHKERR mField.loop_entities("PHI0", surf_dist);
-    CHKERR mField.getInterface<FieldBlas>()->fieldCopy(1, "PHI0", "PHI");
-    MoFEMFunctionReturn(0);
-  }
-
-private:
-  MoFEM::Interface &mField;
-  boost::shared_ptr<OrientedBoxTreeTool> treeSurfPtr;
-  EntityHandle rootSetSurf;
-
-  struct DistanceFromVertex : public EntityMethod {
-
-    DistanceFromVertex(boost::shared_ptr<OrientedBoxTreeTool> &tree,
-                       EntityHandle root)
-        : EntityMethod(), treeSurfPtr(tree), rootSetSurf(root) {}
-
-    MoFEMErrorCode preProcess() { return 0; }
-
-    MoFEMErrorCode operator()() {
-      MoFEMFunctionBegin;
-      if (entPtr->getEntType() == MBVERTEX) {
-        EntityHandle vert = entPtr->getEnt();
-        VectorDouble3 coords(3);
-        CHKERR entPtr->getBasicDataPtr()->moab.get_coords(&vert, 1,
-                                                          &*coords.begin());
-        // VectorDouble3 point_out(3);
-        // EntityHandle facets_out;
-        // CHKERR treeSurfPtr->closest_to_location(&coords[0], rootSetSurf,
-        //                                         &point_out[0], facets_out);
-        // VectorDouble3 n(3);
-        // Util::normal(&entPtr->getBasicDataPtr()->moab, facets_out, n[0], n[1],
-        //              n[2]);
-        // n /= norm_2(n);
-        // VectorDouble3 delta = point_out - coords;
-        entPtr->getEntFieldData()[0] = inner_prod(coords, coords) - 9.;
-      }
-      MoFEMFunctionReturn(0);
-    };
-
-    MoFEMErrorCode postProcess() { return 0; }
-
-  private:
-    boost::shared_ptr<OrientedBoxTreeTool> treeSurfPtr;
-    EntityHandle rootSetSurf;
-  };
-};
-
 struct CommonData {
 
   MatrixDouble grad;
   VectorDouble val;
+  VectorDouble dot_val;
+  MatrixDouble invJac;
 
   SmartPetscObj<Mat> M;
   SmartPetscObj<KSP> ksp;
@@ -83,12 +28,13 @@ struct CommonData {
 
 struct OpCalMass : OpFaceEle {
   OpCalMass(boost::shared_ptr<CommonData> &data)
-      : OpFaceEle("PHI", "PHI", OpFaceEle::OPROWCOL), commonData(data) {}
+      : OpFaceEle("u", "u", OpFaceEle::OPROWCOL), commonData(data) {
+    sYmm = true;
+  }
   MoFEMErrorCode doWork(int row_side, int col_side, EntityType row_type,
                         EntityType col_type, EntData &row_data,
                         EntData &col_data) {
     MoFEMFunctionBegin;
-    FTensor::Index<'i', 3> i;
     const int nb_row_dofs = row_data.getIndices().size();
     const int nb_col_dofs = col_data.getIndices().size();
     if (nb_row_dofs && nb_col_dofs) {
@@ -97,7 +43,7 @@ struct OpCalMass : OpFaceEle {
       mat.clear();
       auto t_row_base = row_data.getFTensor0N();
       auto t_w = getFTensor0IntegrationWeight();
-      const double vol = getVolume();
+      const double vol = getMeasure();
       for (int gg = 0; gg != nb_integration_pts; ++gg) {
         const double a = t_w * vol;
         for (int rr = 0; rr != nb_row_dofs; ++rr) {
@@ -113,22 +59,26 @@ struct OpCalMass : OpFaceEle {
       CHKERR MatSetValues(
           commonData->M, nb_row_dofs, &*row_data.getIndices().begin(),
           nb_col_dofs, &*col_data.getIndices().begin(), &mat(0, 0), ADD_VALUES);
+      if (row_side != col_side || row_type != col_type) {
+        transMat.resize(nb_col_dofs, nb_row_dofs, false);
+        noalias(transMat) = trans(mat);
+        CHKERR MatSetValues(commonData->M, nb_col_dofs,
+                            &*col_data.getIndices().begin(), nb_row_dofs,
+                            &*row_data.getIndices().begin(), &transMat(0, 0),
+                            ADD_VALUES);
+      }
     }
     MoFEMFunctionReturn(0);
   }
 
 private:
-  MatrixDouble mat;
+  MatrixDouble mat, transMat;
   boost::shared_ptr<CommonData> commonData;
 };
 
-struct OpAssemble : OpFaceEle {
-  OpAssemble(boost::shared_ptr<CommonData> &data)
-      : OpFaceEle("PHI", OpFaceEle::OPROW), commonData(data) {
-    doEdges = false;
-    doTris = false;
-    doTets = false;
-  }
+struct OpAssembleSlowRhs : OpFaceEle {
+  OpAssembleSlowRhs(boost::shared_ptr<CommonData> &data)
+      : OpFaceEle("u", OpFaceEle::OPROW), commonData(data) {}
   VectorDouble vecF;
   MoFEMErrorCode doWork(int side, EntityType type, EntData &data) {
     MoFEMFunctionBegin;
@@ -139,30 +89,124 @@ struct OpAssemble : OpFaceEle {
 
       const int nb_integration_pts = getGaussPts().size2();
       auto t_val = getFTensor0FromVec(commonData->val);
-      auto t_grad = getFTensor1FromMat<3>(commonData->grad);
       auto t_base = data.getFTensor0N();
-      auto t_diff_base = data.getFTensor1DiffN<3>();
       auto t_w = getFTensor0IntegrationWeight();
-      FTensor::Index<'i', 3> i;
 
-      const double vol = getVolume();
+      const double vol = getMeasure();
       for (int gg = 0; gg != nb_integration_pts; ++gg) {
         const double a = vol * t_w;
-        const double f = -a * t_val * (1 - t_val);
+        const double f = a * t_val * (1 - t_val);
         for (int rr = 0; rr != nb_dofs; ++rr) {
-          const double b = f * t_base + a * t_diff_base(i) * t_grad(i);
+          const double b = f * t_base;
           vecF[rr] += b;
           ++t_base;
-          ++t_diff_base;
         }
 
         ++t_val;
+        ++t_w;
+      }
+
+      CHKERR VecSetOption(getFEMethod()->ts_F, VEC_IGNORE_NEGATIVE_INDICES,
+                          PETSC_TRUE);
+      CHKERR VecSetValues(getFEMethod()->ts_F, data, &*vecF.begin(),
+                          ADD_VALUES);
+    }
+    MoFEMFunctionReturn(0);
+  }
+
+private:
+  MatrixDouble mat, transMat;
+  boost::shared_ptr<CommonData> commonData;
+};
+
+template <int DIM> struct OpAssembleStiffRhs : OpFaceEle {
+  OpAssembleStiffRhs(boost::shared_ptr<CommonData> &data)
+      : OpFaceEle("u", OpFaceEle::OPROW), commonData(data) {}
+  VectorDouble vecF;
+  MoFEMErrorCode doWork(int side, EntityType type, EntData &data) {
+    MoFEMFunctionBegin;
+    const int nb_dofs = data.getIndices().size();
+    if (nb_dofs) {
+      vecF.resize(nb_dofs, false);
+      vecF.clear();
+
+      const int nb_integration_pts = getGaussPts().size2();
+      auto t_dot_val = getFTensor0FromVec(commonData->dot_val);
+      auto t_grad = getFTensor1FromMat<DIM>(commonData->grad);
+      auto t_base = data.getFTensor0N();
+      auto t_diff_base = data.getFTensor1DiffN<DIM>();
+      auto t_w = getFTensor0IntegrationWeight();
+      FTensor::Index<'i', DIM> i;
+
+      const double vol = getMeasure();
+      for (int gg = 0; gg != nb_integration_pts; ++gg) {
+        const double a = vol * t_w;
+        for (int rr = 0; rr != nb_dofs; ++rr) {
+          const double b =
+              t_base * t_dot_val + 1e-3 * (a * t_diff_base(i) * t_grad(i));
+          vecF[rr] += b;
+          ++t_diff_base;
+          ++t_base;
+        }
         ++t_grad;
         ++t_w;
       }
 
-      CHKERR VecSetValues(getFEMethod()->ts_F, nb_dofs,
-                          &*data.getIndices().begin(), &*vecF.begin(),
+      CHKERR VecSetOption(getFEMethod()->ts_F, VEC_IGNORE_NEGATIVE_INDICES,
+                          PETSC_TRUE);
+      CHKERR VecSetValues(getFEMethod()->ts_F, data, &*vecF.begin(),
+                          ADD_VALUES);
+    }
+    MoFEMFunctionReturn(0);
+  }
+
+private:
+  boost::shared_ptr<CommonData> commonData;
+};
+
+template <int DIM> struct OpAssembleStiffLhs : OpFaceEle {
+  OpAssembleStiffLhs(boost::shared_ptr<CommonData> &data)
+      : OpFaceEle("u", "u", OpFaceEle::OPROWCOL), commonData(data) {}
+  MatrixDouble mat;
+  MoFEMErrorCode doWork(int row_side, int col_side, EntityType row_type,
+                        EntityType col_type, EntData &row_data,
+                        EntData &col_data) {
+    MoFEMFunctionBegin;
+    const int nb_row_dofs = row_data.getIndices().size();
+    const int nb_col_dofs = col_data.getIndices().size();
+    if (nb_row_dofs && nb_col_dofs) {
+      mat.resize(nb_row_dofs, nb_col_dofs, false);
+      mat.clear();
+
+      const int nb_integration_pts = getGaussPts().size2();
+      auto t_row_base = row_data.getFTensor0N();
+      auto t_row_diff_base = row_data.getFTensor1DiffN<DIM>();
+      auto t_w = getFTensor0IntegrationWeight();
+      FTensor::Index<'i', DIM> i;
+
+      const double ts_a = getFEMethod()->ts_a;
+      const double vol = getMeasure();
+      for (int gg = 0; gg != nb_integration_pts; ++gg) {
+        const double a = vol * t_w;
+        for (int rr = 0; rr != nb_row_dofs; ++rr) {
+
+          auto t_col_base = col_data.getFTensor0N(gg, 0);
+          auto t_col_diff_base = col_data.getFTensor1DiffN<DIM>(gg, 0);
+
+          for (int cc = 0; cc != nb_col_dofs; ++cc) {
+            mat(rr, cc) += a * t_row_base * t_col_base * ts_a +
+                           1e-3 * t_row_diff_base(i) * t_col_diff_base(i);
+            ++t_col_base;
+            ++t_col_diff_base;
+          }
+
+          ++t_row_base;
+          ++t_row_diff_base;
+        }
+        ++t_w;
+      }
+
+      CHKERR MatSetValues(getFEMethod()->ts_B, row_data, col_data, &mat(0, 0),
                           ADD_VALUES);
     }
     MoFEMFunctionReturn(0);
@@ -179,14 +223,6 @@ int main(int argc, char *argv[]) {
 
   try {
 
-    int surface_side_set = 200;
-
-    CHKERR PetscOptionsBegin(PETSC_COMM_WORLD, "", "Level set", "none");
-    CHKERR PetscOptionsInt("-surface_side_set", "surface side set", "",
-                           surface_side_set, &surface_side_set, PETSC_NULL);
-    ierr = PetscOptionsEnd();
-    CHKERRG(ierr);
-
     moab::Core mb_instance;
     moab::Interface &moab = mb_instance;
     MoFEM::Core core(moab);
@@ -200,65 +236,112 @@ int main(int argc, char *argv[]) {
     Simple *simple_interface;
     CHKERR m_field.getInterface(simple_interface);
     CHKERR simple_interface->getOptions();
-    CHKERR simple_interface->loadFile("");
+    CHKERR simple_interface->loadFile();
 
     // add fields
-    CHKERR simple_interface->addDomainField("PHI", H1, AINSWORTH_LEGENDRE_BASE,
+    CHKERR simple_interface->addDomainField("u", H1, AINSWORTH_LEGENDRE_BASE,
                                             1);
-    CHKERR simple_interface->addDataField("PHI0", H1, AINSWORTH_LEGENDRE_BASE,
-                                          1);
     // set fields order
-    CHKERR simple_interface->setFieldOrder("PHI", 1);
-    CHKERR simple_interface->setFieldOrder("PHI0", 1);
+    CHKERR simple_interface->setFieldOrder("u", 1);
     // setup problem
-    CHKERR simple_interface->setUp(PETSC_FALSE);
-
-    // get surface entities form side set
-    Range surface;
-    if (m_field.getInterface<MeshsetsManager>()->checkMeshset(surface_side_set,
-                                                              SIDESET))
-      CHKERR m_field.getInterface<MeshsetsManager>()->getEntitiesByDimension(
-          surface_side_set, SIDESET, 2, surface, true);
-    if (surface.empty())
-      SETERRQ(PETSC_COMM_WORLD, MOFEM_ATOM_TEST_INVALID, "No surface");
+    CHKERR simple_interface->setUp();
 
     boost::shared_ptr<CommonData> data(new CommonData());
     auto val_ptr = boost::shared_ptr<VectorDouble>(data, &data->val);
+    auto dot_val_ptr = boost::shared_ptr<VectorDouble>(data, &data->dot_val);
     auto grad_ptr = boost::shared_ptr<MatrixDouble>(data, &data->grad);
 
-    boost::shared_ptr<FaceEle> vol_ele(new FaceEle(m_field));
-    vol_ele->getOpPtrVector().push_back(
-        new OpCalculateScalarFieldValues("PHI0", val_ptr));
-    vol_ele->getOpPtrVector().push_back(
-        new OpCalculateScalarFieldGradient<3>("PHI", grad_ptr));
-    vol_ele->getOpPtrVector().push_back(new OpAssemble(data));
+    boost::shared_ptr<FaceEle> vol_ele_slow_rhs(new FaceEle(m_field));
+    boost::shared_ptr<FaceEle> vol_ele_stiff_rhs(new FaceEle(m_field));
+    boost::shared_ptr<FaceEle> vol_ele_stiff_lhs(new FaceEle(m_field));
+
+    vol_ele_slow_rhs->getOpPtrVector().push_back(
+        new OpCalculateInvJacForFace(data->invJac));
+    vol_ele_slow_rhs->getOpPtrVector().push_back(
+        new OpSetInvJacH1ForFace(data->invJac));
+    vol_ele_slow_rhs->getOpPtrVector().push_back(
+        new OpCalculateScalarFieldValues("u", val_ptr));
+    vol_ele_slow_rhs->getOpPtrVector().push_back(
+        new OpCalculateScalarFieldGradient<2>("u", grad_ptr));
+    vol_ele_slow_rhs->getOpPtrVector().push_back(new OpAssembleSlowRhs(data));
+
+    vol_ele_stiff_rhs->getOpPtrVector().push_back(
+        new OpCalculateInvJacForFace(data->invJac));
+    vol_ele_stiff_rhs->getOpPtrVector().push_back(
+        new OpSetInvJacH1ForFace(data->invJac));
+    vol_ele_stiff_rhs->getOpPtrVector().push_back(
+        new OpCalculateScalarFieldValues("u", val_ptr));
+    vol_ele_stiff_rhs->getOpPtrVector().push_back(
+        new OpCalculateVectorScalarValuesDot("u", dot_val_ptr));
+    vol_ele_stiff_rhs->getOpPtrVector().push_back(
+        new OpCalculateScalarFieldGradient<2>("u", grad_ptr));
+    vol_ele_stiff_rhs->getOpPtrVector().push_back(
+        new OpAssembleStiffRhs<2>(data));
+
+    vol_ele_stiff_lhs->getOpPtrVector().push_back(
+        new OpCalculateInvJacForFace(data->invJac));
+    vol_ele_stiff_lhs->getOpPtrVector().push_back(
+        new OpSetInvJacH1ForFace(data->invJac));
+    vol_ele_stiff_lhs->getOpPtrVector().push_back(
+        new OpAssembleStiffLhs<2>(data));
+
     boost::shared_ptr<FaceEle> vol_mass_ele(new FaceEle(m_field));
     vol_mass_ele->getOpPtrVector().push_back(new OpCalMass(data));
 
     auto vol_rule = [](int, int, int p) -> int { return 2 * p; };
-    vol_ele->getRuleHook = vol_rule;
+    vol_ele_slow_rhs->getRuleHook = vol_rule;
+    vol_ele_stiff_rhs->getRuleHook = vol_rule;
+    vol_ele_stiff_lhs->getRuleHook = vol_rule;
     vol_mass_ele->getRuleHook = vol_rule;
 
-    boost::shared_ptr<PostProcVolumeOnRefinedMesh> post_proc_volume =
-        boost::shared_ptr<PostProcVolumeOnRefinedMesh>(
-            new PostProcVolumeOnRefinedMesh(m_field));
+    boost::shared_ptr<PostProcFaceOnRefinedMesh> post_proc =
+        boost::shared_ptr<PostProcFaceOnRefinedMesh>(
+            new PostProcFaceOnRefinedMesh(m_field));
     boost::shared_ptr<ForcesAndSourcesCore> null;
 
-    post_proc_volume->generateReferenceElementMesh();
-    post_proc_volume->addFieldValuesPostProc("PHI");
-    post_proc_volume->addFieldValuesGradientPostProc("PHI");
-    post_proc_volume->addFieldValuesPostProc("PHI0");
-    post_proc_volume->addFieldValuesGradientPostProc("PHI0");
+    post_proc->generateReferenceElementMesh();
+    post_proc->addFieldValuesPostProc("u");
 
     auto dm = simple_interface->getDM();
 
-    CHKERR CalculateDistantFromSurface(m_field)(surface);
+    // get surface entities form side set
+    Range inner_surface;
+    if (m_field.getInterface<MeshsetsManager>()->checkMeshset(1, BLOCKSET))
+      CHKERR m_field.getInterface<MeshsetsManager>()->getEntitiesByDimension(
+          1, BLOCKSET, 2, inner_surface, true);
+    if (!inner_surface.empty()) {
+      Range inner_surface_verts;
+      CHKERR moab.get_connectivity(inner_surface, inner_surface_verts, false);
+      CHKERR m_field.getInterface<FieldBlas>()->setField(
+          0.1, MBVERTEX, inner_surface_verts, "u");
+    }
+
+    Range surface;
+    CHKERR moab.get_entities_by_type(0, MBTRI, surface, false);
+    Skinner skin(&m_field.get_moab());
+    Range edges;
+    CHKERR skin.find_skin(0, surface, false, edges);
+    Range edges_part;
+    ParallelComm *pcomm = ParallelComm::get_pcomm(&moab, MYPCOMM_INDEX);
+    CHKERR pcomm->filter_pstatus(edges_part,
+                                 PSTATUS_SHARED | PSTATUS_MULTISHARED,
+                                 PSTATUS_NOT, -1, &edges);
+    Range edges_verts;
+    CHKERR moab.get_connectivity(edges_part, edges_verts, false);
+    CHKERR m_field.getInterface<ProblemsManager>()->removeDofsOnEntities(
+        simple_interface->getProblemName(), "u",
+        unite(edges_verts, edges_part));
 
     auto ts = createTS(m_field.get_comm());
-    CHKERR TSSetType(ts, TSRK);
+    CHKERR TSSetType(ts, TSEIMEX);
+    // CHKERR TSSetType(ts, TSRK);
     // CHKERR TSSetType(ts, TSEULER);
+    CHKERR DMMoFEMTSSetIJacobian(dm, simple_interface->getDomainFEName(),
+                                 vol_ele_stiff_lhs, null, null);
+    CHKERR DMMoFEMTSSetIFunction(dm, simple_interface->getDomainFEName(),
+                                 vol_ele_stiff_rhs, null, null);
     CHKERR DMMoFEMTSSetRHSFunction(dm, simple_interface->getDomainFEName(),
-                                   vol_ele, null, null);
+                                   vol_ele_slow_rhs, null, null);
 
     SmartPetscObj<Vec> X;
     CHKERR DMCreateGlobalVector_MoFEM(dm, X);
@@ -275,22 +358,24 @@ int main(int argc, char *argv[]) {
     CHKERR KSPSetFromOptions(data->ksp);
     CHKERR KSPSetUp(data->ksp);
 
-    auto solve_mass = [&]() {
-      MoFEMFunctionBegin;
+    // auto solve_mass = [&]() {
+    //   MoFEMFunctionBegin;
 
-      if (vol_ele->vecAssembleSwitch) {
-        CHKERR VecGhostUpdateBegin(vol_ele->ts_F, ADD_VALUES, SCATTER_REVERSE);
-        CHKERR VecGhostUpdateEnd(vol_ele->ts_F, ADD_VALUES, SCATTER_REVERSE);
-        CHKERR VecAssemblyBegin(vol_ele->ts_F);
-        CHKERR VecAssemblyEnd(vol_ele->ts_F);
-        *vol_ele->vecAssembleSwitch = false;
-      }
+    //   if (vol_ele_slow->vecAssembleSwitch) {
+    //     CHKERR VecGhostUpdateBegin(vol_ele_slow->ts_F, ADD_VALUES,
+    //                                SCATTER_REVERSE);
+    //     CHKERR VecGhostUpdateEnd(vol_ele_slow->ts_F, ADD_VALUES,
+    //                              SCATTER_REVERSE);
+    //     CHKERR VecAssemblyBegin(vol_ele_slow->ts_F);
+    //     CHKERR VecAssemblyEnd(vol_ele_slow->ts_F);
+    //     *vol_ele_slow->vecAssembleSwitch = false;
+    //   }
 
-      CHKERR KSPSolve(data->ksp, vol_ele->ts_F, vol_ele->ts_F);
+    //   CHKERR KSPSolve(data->ksp, vol_ele_slow->ts_F, vol_ele_slow->ts_F);
 
-      MoFEMFunctionReturn(0);
-    };
-    vol_ele->postProcessHook = solve_mass;
+    //   MoFEMFunctionReturn(0);
+    // };
+    // vol_ele_slow->postProcessHook = solve_mass;
 
     CHKERR DMoFEMMeshToLocalVector(dm, X, INSERT_VALUES, SCATTER_FORWARD);
 
@@ -305,8 +390,8 @@ int main(int argc, char *argv[]) {
     CHKERR VecGhostUpdateEnd(X, INSERT_VALUES, SCATTER_FORWARD);
     CHKERR DMoFEMMeshToGlobalVector(dm, X, INSERT_VALUES, SCATTER_REVERSE);
     CHKERR DMoFEMLoopFiniteElements(dm, simple_interface->getDomainFEName(),
-                                    post_proc_volume);
-    CHKERR post_proc_volume->writeFile("out_level.h5m");
+                                    post_proc);
+    CHKERR post_proc->writeFile("out_level.h5m");
   }
   CATCH_ERRORS;
 
