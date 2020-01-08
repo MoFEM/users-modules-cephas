d79607519f06fa699bee7021a8a0f6a549d1e451
Lukasz Kaczmarczyk <likask@wp.pl>
2020-01-08 19:12:42 +0000

    work in progress contact

--- basic_finite_elements/basic/src/ContactOps.hpp
+++ basic_finite_elements/basic/src/ContactOps.hpp
@@ -45,6 +45,26 @@ private:
   boost::shared_ptr<CommonData> commonDataPtr;
 };
 
+struct OpConstrainTraction : public BoundaryEleOp {
+  OpConstrainTraction(const std::string field_name,
+                 boost::shared_ptr<CommonData> common_data_ptr);
+  MoFEMErrorCode doWork(int side, EntityType type,
+                        DataForcesAndSourcesCore::EntData &data);
+
+private:
+  boost::shared_ptr<CommonData> commonDataPtr;
+};
+
+struct OpConstrainDisp : public BoundaryEleOp {
+  OpConstrainDisp(const std::string field_name,
+                 boost::shared_ptr<CommonData> common_data_ptr);
+  MoFEMErrorCode doWork(int side, EntityType type,
+                        DataForcesAndSourcesCore::EntData &data);
+
+private:
+  boost::shared_ptr<CommonData> commonDataPtr;
+};
+
 template <typename T>
 inline double gap(FTensor::Tensor1<T, 2> &t_disp,
                   FTensor::Tensor1<double, 2> &t_normal) {
@@ -199,5 +219,97 @@ MoFEMErrorCode OpConstrainRhs::doWork(int side, EntityType type,
 
   MoFEMFunctionReturn(0);
 }
+
+OpConstrainTraction::OpConstrainTraction(
+    const std::string field_name, boost::shared_ptr<CommonData> common_data_ptr)
+    : BoundaryEleOp(field_name, DomianEleOp::OPROW),
+      commonDataPtr(common_data_ptr) {
+  std::fill(&doEntities[MBVERTEX], &doEntities[MBMAXTYPE], false);
+  doEntities[MBEDGE] = true;
+}
+
+MoFEMErrorCode OpConstrainTraction::doWork(int side, EntityType type,
+                                      DataForcesAndSourcesCore::EntData &data) {
+
+  MoFEMFunctionBegin;
+
+  const size_t nb_gauss_pts = commonDataPtr->mStrainPtr->size2();
+  const size_t nb_dofs = data.getIndices().size();
+
+  if (nb_dofs) {
+
+    std::array<double, MAX_DOFS_ON_ENTITY> nf;
+    std::fill(&nf[0], &nf[nb_dofs], 0);
+
+    commonDataPtr->contactTractionPtr->resize(2, nb_gauss_pts);
+    commonDataPtr->contactTractionPtr->clear();
+
+    auto t_direction = getFTensor1Direction();
+    FTensor::Tensor1<double, 2> t_normal{t_direction(0), t_direction(1)};
+    const double l = sqrt(t_normal(i) * t_normal(i));
+    t_normal(i) /= l;
+
+    auto t_traction =
+        getFTensor1FromMat<2>(*(commonDataPtr->contactTractionPtr));
+
+    auto t_base = data.getFTensor1N<3>();
+    for (size_t gg = 0; gg != nb_gauss_pts; ++gg) {
+
+      auto t_field_data = data.getFTensor1FieldData<2>();
+      for (size_t bb = 0; bb != nb_dofs / 2; ++bb) {
+        t_traction(j) += (t_base(i) * t_normal(i)) * t_field_data(j);
+        ++t_field_data;
+        ++t_base;
+      };
+      ++t_traction;
+    }
+  }
+
+  MoFEMFunctionReturn(0);
+}
+
+OpConstrainDisp::OpConstrainDisp(
+    const std::string field_name, boost::shared_ptr<CommonData> common_data_ptr)
+    : BoundaryEleOp(field_name, DomianEleOp::OPROW),
+      commonDataPtr(common_data_ptr) {
+  std::fill(&doEntities[MBTRI], &doEntities[MBMAXTYPE], false);
+}
+
+MoFEMErrorCode OpConstrainDisp::doWork(int side, EntityType type,
+                                      DataForcesAndSourcesCore::EntData &data) {
+
+  MoFEMFunctionBegin;
+
+  const size_t nb_gauss_pts = commonDataPtr->mStrainPtr->size2();
+  const size_t nb_dofs = data.getIndices().size();
+
+  if (nb_dofs) {
+
+    std::array<double, MAX_DOFS_ON_ENTITY> nf;
+    std::fill(&nf[0], &nf[nb_dofs], 0);
+
+    commonDataPtr->contactDispPtr->resize(2, nb_gauss_pts);
+    commonDataPtr->contactDispPtr->clear();
+
+    auto t_disp = getFTensor1FromMat<2>(*(commonDataPtr->contactDispPtr));
+
+    auto t_base = data.getFTensor0N();
+    for (size_t gg = 0; gg != nb_gauss_pts; ++gg) {
+
+      auto t_field_data = data.getFTensor1FieldData<2>();
+      for (size_t bb = 0; bb != nb_dofs / 2; ++bb) {
+        t_disp(j) += t_base * t_field_data(j);
+        ++t_field_data;
+        ++t_base;
+      };
+      ++t_disp;
+    }
+  }
+
+  MoFEMFunctionReturn(0);
+}
+
+
+
 
 }; // namespace OpContactTools
