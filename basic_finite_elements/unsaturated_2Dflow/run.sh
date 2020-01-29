456528bd29ab1b9b891183bb448fec85a37d81ab
Mebratu Wakeni <mebratu.wakeni@glasgow.ac.uk>
2020-01-29 17:29:42 +0000

    Exercises on documentation added

--- basic_finite_elements/unsaturated_2Dflow/run.sh
+++ basic_finite_elements/unsaturated_2Dflow/run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
   
-../../tools/add_meshsets -my_file paper_network.msh -meshsets_config bc.cfg
+if  [[ $1 = "-my_file" ]]; then
+    echo "Running with input file $2 ..."
+    ../../tools/add_meshsets -my_file $2 -meshsets_config bc.cfg 
+else
+  echo "Give the input mesh file name ..."  
+  exit
+fi
 
 cp out.h5m paper_network_pre.h5m
 
 ../../tools/mofem_part -my_file paper_network_pre.h5m -output_file paper_network_final.h5m -my_nparts 1 -dim 2 -adj_dim 1
 
-time mpirun -np 1 ./unsatu2dFlow_prob 2>&1 | tee log
+time mpirun -np 1 ./unsatu2dFlow_prob -file_name paper_network_final.h5m 2>&1 | tee log
 
 ../nonlinear_elasticity/do_vtk.sh out_*.h5m
