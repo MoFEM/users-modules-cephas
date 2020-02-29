a035222fe2404482c717bdb5717605d7356c1414
Lukasz Kaczmarczyk <likask@wp.pl>
2020-02-29 22:40:06 +0000

    update file copying

--- basic_finite_elements/reaction_diffusion_equation/doc/AddDocumentation.cmake
+++ basic_finite_elements/reaction_diffusion_equation/doc/AddDocumentation.cmake
@@ -14,6 +14,5 @@
 # copy dox/figures to html directory created by doxygen
 add_custom_target(reaction_diffusion_imp
   ${CMAKE_COMMAND} -E copy_directory
-  ${PROJECT_SOURCE_DIR}/users_modules/basic_finite_elements/reaction_diffusion_equation/doc/figures ${PROJECT_BINARY_DIR}/html
-)
+  ${ADD_DOC_DIRECTORY}/figures ${PROJECT_BINARY_DIR}/html)
 add_dependencies(doc reaction_diffusion_imp)
