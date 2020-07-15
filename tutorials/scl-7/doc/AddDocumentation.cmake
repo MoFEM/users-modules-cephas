a6975741e48d7ce2b28b35696755111b93325c12
Hoang Nguyen <hoangnguyence@gmail.com>
2020-07-16 00:00:55 +0100

    Small fix

--- tutorials/scl-7/doc/AddDocumentation.cmake
+++ tutorials/scl-7/doc/AddDocumentation.cmake
@@ -12,7 +12,7 @@
 # License along with MoFEM. If not, see <http://www.gnu.org/licenses/>
 
 # copy dox/figures to html directory created by doxygen
-add_custom_target(basic_tutorials_scl_6
+add_custom_target(basic_tutorials_scl_7
   ${CMAKE_COMMAND} -E copy_directory
   ${ADD_DOC_DIRECTORY}/figures ${PROJECT_BINARY_DIR}/html)
-add_dependencies(doc basic_tutorials_scl_6)
+add_dependencies(doc basic_tutorials_scl_7)
