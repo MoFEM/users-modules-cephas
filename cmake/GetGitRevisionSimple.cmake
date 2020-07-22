3070a6ca09f5488c610ef8bd6b7f37811319f95e
Lukasz Kaczmarczyk <likask@wp.pl>
2020-07-22 14:07:47 +0100

    fix force cashe

--- cmake/GetGitRevisionSimple.cmake
+++ cmake/GetGitRevisionSimple.cmake
@@ -21,9 +21,9 @@ function(get_git_hash GIT_DIR _hashvar)
     RESULT_VARIABLE res)
   if(NOT ${res})
     string(REGEX REPLACE "\n$" "" HEAD_HASH "${HEAD_HASH}")
-    set(${_hashvar} "${HEAD_HASH}" PARENT_SCOPE CACHE STRING "Git hash" FORCE)
+    set(${_hashvar} "${HEAD_HASH}" PARENT_SCOPE)
   else(NOT ${res})
-    set(${_hashvar} "SHA1-NOT FOUND" PARENT_SCOPE CACHE STRING "Git hash" FORCE)
+    set(${_hashvar} "SHA1-NOT FOUND" PARENT_SCOPE)
   endif(NOT ${res})
 endfunction()
 
@@ -35,11 +35,9 @@ function(get_git_tag GIT_DIR FALLBACK _gittag)
     RESULT_VARIABLE res)
   if(NOT ${res})
     string(REGEX REPLACE "\n$" "" GIT_TAG "${GIT_TAG}")
-    set(${_gittag} "${GIT_TAG}" PARENT_SCOPE CACHE STRING "Git tag" FORCE)
+    set(${_gittag} "${GIT_TAG}" PARENT_SCOPE)
   else(NOT ${res})
-    set(
-      ${_gittag} "${FALLBACK}-fallback" PARENT_SCOPE 
-      CACHE STRING "Git tag" FORCE)
+    set(${_gittag} "${FALLBACK}-fallback" PARENT_SCOPE)
   endif(NOT ${res}) 
 endfunction()
 
@@ -51,11 +49,7 @@ function(get_git_version
     "^v[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR "${GIT_TAG_VERSION}")
   string(REGEX REPLACE 
     "^v[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" VERSION_BUILD "${GIT_TAG_VERSION}")
-  set(
-    ${_version_major} "${VERSION_MAJOR}" PARENT_SCOPE 
-    CACHE STRING "Major version" FORCE)
-  set(${_version_minor} "${VERSION_MINOR}" PARENT_SCOPE
-    CACHE STRING "Mainor version" FORCE)
-  set(${_version_build} "${VERSION_BUILD}" PARENT_SCOPE
-    CACHE STRING "Build version" FORCE)
+  set(${_version_major} "${VERSION_MAJOR}" PARENT_SCOPE)
+  set(${_version_minor} "${VERSION_MINOR}" PARENT_SCOPE)
+  set(${_version_build} "${VERSION_BUILD}" PARENT_SCOPE)
 endfunction()
