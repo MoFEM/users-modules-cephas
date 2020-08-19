b34c517864f3bcb7d6420e499045cc2259c6cf71
Lukasz Kaczmarczyk <likask@wp.pl>
2020-08-19 10:00:52 +0100

    add note from jira

--- basic_finite_elements/lessons/lesson3_poisson.cpp
+++ basic_finite_elements/lessons/lesson3_poisson.cpp
@@ -21,6 +21,8 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with MoFEM. If not, see <http://www.gnu.org/licenses/>. */
 
+// TODO: [CORE-62] Improve method of enforcing boundary condition 
+
 #include <MoFEM.hpp>
 
 using namespace MoFEM;
