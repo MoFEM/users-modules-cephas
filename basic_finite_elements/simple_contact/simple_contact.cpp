70b38f42fac9b3eea9f445b44e816801da372cd7
Andrei Shvarts <andrei.shvarts@glasgow.ac.uk>
2020-11-26 12:53:41 +0000

    use default value

--- basic_finite_elements/simple_contact/simple_contact.cpp
+++ basic_finite_elements/simple_contact/simple_contact.cpp
@@ -761,7 +761,7 @@ int main(int argc, char *argv[]) {
 
     contact_problem->setContactOperatorsForPostProc(
         fe_post_proc_simple_contact, common_data_simple_contact, m_field,
-        "SPATIAL_POSITION", "LAGMULT", mb_post, alm_flag, true);
+        "SPATIAL_POSITION", "LAGMULT", mb_post, alm_flag);
 
     mb_post.delete_mesh();
 
