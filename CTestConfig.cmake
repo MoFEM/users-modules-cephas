785bffa4cdd9ec5d7d30b15f35f571f63bfb288a
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2021-11-16 12:46:27 +0000

    update cdash file
    (cherry picked from commit 64fe8aab5e29d5ba9b23e3b2ba8c01a3d3f9a184)

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,13 +1,15 @@
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
-## # The following are required to uses Dart and the Cdash dashboard
+##
+## # The following are required to submit to the CDash dashboard:
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
-set(CTEST_PROJECT_NAME "Cephas-usersmodules")
+
+set(CTEST_PROJECT_NAME "usersmodules-mofem")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "cdash.eng.gla.ac.uk")
-set(CTEST_DROP_LOCATION "/cdash/submit.php?project=Cephas-usersmodules")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=usersmodules-mofem")
 set(CTEST_DROP_SITE_CDASH TRUE)
