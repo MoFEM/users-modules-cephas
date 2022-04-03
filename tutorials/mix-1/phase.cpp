f237d2ff31349be33b5b6e8d89737c1590226863
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2022-04-03 18:20:38 +0100

    dead end

--- tutorials/mix-1/phase.cpp
+++ tutorials/mix-1/phase.cpp
@@ -67,7 +67,8 @@ const int *d1_savitzky_golay_p4[] = {nullptr,
                                      d1_savitzky_golay_w9_p4.data()};
 
 using EntData = EntitiesFieldData::EntData;
-
+using AssemblyBoundaryEleOp =
+    FormsIntegrators<EdgeEleOp>::Assembly<PETSC>::OpBase;
 using OpHdivHdiv = FormsIntegrators<DomainEleOp>::Assembly<PETSC>::BiLinearForm<
     GAUSS>::OpMass<3, 3>;
 using OpHdivU = FormsIntegrators<DomainEleOp>::Assembly<PETSC>::BiLinearForm<
@@ -91,9 +92,10 @@ private:
   MoFEMErrorCode readMesh();
   MoFEMErrorCode setupProblem();
   MoFEMErrorCode boundaryCondition();
-  MoFEMErrorCode assembleSystem();
+  MoFEMErrorCode assembleSystemIntensity();
+  MoFEMErrorCode assembleSystemFlux();
   MoFEMErrorCode solveSystem();
-  MoFEMErrorCode calculateLambda();
+  MoFEMErrorCode calculateFlux(double &calc_flux);
   MoFEMErrorCode outputResults(const int i);
 
   enum BoundingBox {
@@ -115,6 +117,12 @@ private:
 
   static double rhsSource(const double x, const double y, const double);
   static double lhsFlux(const double x, const double y, const double);
+
+  SmartPetscObj<Mat> B;
+  SmartPetscObj<Vec> F;
+
+  struct BoundaryOp;
+  struct BoundaryRhs;
 };
 
 std::vector<double> Example::rZ;
@@ -156,20 +164,69 @@ double Example::lhsFlux(const double x, const double y, const double) {
   return 1. / m(idx.first, idx.second);
 }
 
+struct Example::BoundaryOp : public EdgeEleOp {
+  BoundaryOp(boost::shared_ptr<MatrixDouble> flux_ptr, double &glob_flux)
+      : EdgeEleOp(NOSPACE, OPLAST), fluxPtr(flux_ptr), globFlux(glob_flux) {}
+
+  MoFEMErrorCode doWork(int side, EntityType type, EntData &data) {
+    MoFEMFunctionBegin;
+    const auto nb_gauss_pts = getGaussPts().size2();
+    auto t_flux = getFTensor1FromMat<3>(*fluxPtr);
+    auto t_normal = getFTensor1Normal();
+    auto t_w = getFTensor0IntegrationWeight();
+    FTensor::Index<'i', 3> i;
+    for (auto gg = 0; gg != nb_gauss_pts; ++gg) {
+      globFlux += t_w * t_normal(i) * t_flux(i);
+      ++t_flux;
+      ++t_w;
+    };
+    MoFEMFunctionReturn(0);
+  }
+
+private:
+  boost::shared_ptr<MatrixDouble> fluxPtr;
+  double &globFlux;
+};
+
+struct Example::BoundaryRhs : public AssemblyBoundaryEleOp {
+  BoundaryRhs(const std::string flux_field)
+      : AssemblyBoundaryEleOp(flux_field, flux_field, OPROW) {}
+
+  MoFEMErrorCode iNtegrate(EntData &row_data) {
+    MoFEMFunctionBegin;
+    const size_t nb_base_functions = row_data.getN().size2() / 3;
+    auto t_w = getFTensor0IntegrationWeight();
+    auto t_row_base = row_data.getFTensor1N<3>();
+    auto t_normal = getFTensor1Normal();
+    FTensor::Index<'i', 3> i;
+    for (int gg = 0; gg != nbIntegrationPts; gg++) {
+      const double alpha = t_w;
+      int rr = 0;
+      for (; rr != nbRows; ++rr) {
+        locF[rr] += alpha * t_row_base(i) * t_normal(i);
+        ++t_row_base;
+      }
+      for (; rr < nb_base_functions; ++rr)
+        ++t_row_base;
+      ++t_w; // move to another integration weight
+    }
+    MoFEMFunctionReturn(0);
+  }
+};
+
 //! [run problem]
 MoFEMErrorCode Example::runProblem() {
   MoFEMFunctionBegin;
   CHKERR readMesh();
   CHKERR setupProblem();
   CHKERR boundaryCondition();
-  
+
   for (auto i = 1; i != rZ.size() - 1; ++i) {
 
     MOFEM_LOG_CHANNEL("WORLD");
     MOFEM_LOG("WORLD", Sev::inform) << i << " focal length zR = " << rZ[i];
 
     focalIndex = i;
-    CHKERR assembleSystem();
     CHKERR solveSystem();
     CHKERR outputResults(i);
   }
@@ -301,6 +358,8 @@ MoFEMErrorCode Example::setupProblem() {
   // only base for HCURL has been implemented in 2D. Base vectors for HDIV space
   // are be obtained after rotation of HCURL base vectors by a right angle
   CHKERR simpleInterface->addDomainField("S", HCURL, DEMKOWICZ_JACOBI_BASE, 1);
+  CHKERR simpleInterface->addBoundaryField("S", HCURL, DEMKOWICZ_JACOBI_BASE,
+                                           1);
   // We use AINSWORTH_LEGENDRE_BASE since DEMKOWICZ_JACOBI_BASE for triangle
   // is not yet implemented for L2 space. For quads DEMKOWICZ_JACOBI_BASE and
   // AINSWORTH_LEGENDRE_BASE are construcreed in the same way
@@ -331,16 +390,46 @@ MoFEMErrorCode Example::boundaryCondition() {
 }
 //! [Applying essential BC]
 
+//! [Calculate flux on boundary]
+MoFEMErrorCode Example::calculateFlux(double &calc_flux) {
+  MoFEMFunctionBegin;
+  auto pipeline_mng = mField.getInterface<PipelineManager>();
+
+  pipeline_mng->getDomainLhsFE().reset();
+  pipeline_mng->getDomainRhsFE().reset();
+  pipeline_mng->getBoundaryRhsFE().reset();
+
+  auto rule_vol = [](int, int, int order) { return 2 * (order + 1); };
+  pipeline_mng->setBoundaryRhsIntegrationRule(rule_vol);
+
+  auto flux_ptr = boost::make_shared<MatrixDouble>();
+  pipeline_mng->getOpBoundaryRhsPipeline().push_back(
+      new OpSetContravariantPiolaTransformOnEdge2D());
+  pipeline_mng->getOpBoundaryRhsPipeline().push_back(
+      new OpCalculateHVecVectorField<3>("S", flux_ptr));
+  pipeline_mng->getOpBoundaryRhsPipeline().push_back(
+      new BoundaryOp(flux_ptr, calc_flux));
+
+  calc_flux = 0;
+  CHKERR pipeline_mng->loopFiniteElements();
+  double global_flux_assembeld = 0;
+  MPI_Allreduce(&calc_flux, &global_flux_assembeld, 1, MPI_DOUBLE, MPI_SUM,
+                mField.get_comm());
+  calc_flux = global_flux_assembeld;
+
+  MoFEMFunctionReturn(0);
+}
+//! [Calculate flux on boundary]
+
 //! [Push operators to pipeline]
-MoFEMErrorCode Example::assembleSystem() {
+MoFEMErrorCode Example::assembleSystemIntensity() {
   MoFEMFunctionBegin;
 
   auto *pipeline_mng = mField.getInterface<PipelineManager>();
 
   pipeline_mng->getDomainLhsFE().reset();
   pipeline_mng->getDomainRhsFE().reset();
-  pipeline_mng->getOpDomainRhsPipeline().clear();
-  pipeline_mng->getOpDomainLhsPipeline().clear();
+  pipeline_mng->getBoundaryRhsFE().reset();
 
   auto rule_vol = [](int, int, int order) { return 2 * (order + 1); };
   pipeline_mng->setDomainLhsIntegrationRule(rule_vol);
@@ -368,6 +457,55 @@ MoFEMErrorCode Example::assembleSystem() {
   pipeline_mng->getOpDomainRhsPipeline().push_back(
       new OpDomainSource("PHI", rhsSource));
 
+  auto dm = simpleInterface->getDM();
+  F = smartCreateDMVector(dm);
+  B = smartCreateDMMatrix(dm);
+
+  pipeline_mng->getDomainLhsFE()->ksp_A = B;
+  pipeline_mng->getDomainLhsFE()->ksp_B = B;
+  pipeline_mng->getDomainRhsFE()->ksp_f = F;
+
+  CHKERR pipeline_mng->loopFiniteElements();
+  CHKERR MatAssemblyBegin(B, MAT_FINAL_ASSEMBLY);
+  CHKERR MatAssemblyEnd(B, MAT_FINAL_ASSEMBLY);
+  CHKERR VecAssemblyBegin(F);
+  CHKERR VecAssemblyEnd(F);
+  CHKERR VecGhostUpdateBegin(F, ADD_VALUES, SCATTER_REVERSE);
+  CHKERR VecGhostUpdateEnd(F, ADD_VALUES, SCATTER_REVERSE);
+
+  MoFEMFunctionReturn(0);
+}
+//! [Push operators to pipeline]
+
+//! [Push operators to pipeline]
+MoFEMErrorCode Example::assembleSystemFlux() {
+  MoFEMFunctionBegin;
+
+  auto *pipeline_mng = mField.getInterface<PipelineManager>();
+
+  pipeline_mng->getDomainLhsFE().reset();
+  pipeline_mng->getDomainRhsFE().reset();
+  pipeline_mng->getBoundaryRhsFE().reset();
+
+  auto rule_vol = [](int, int, int order) { return 2 * (order + 1); };
+  pipeline_mng->setBoundaryRhsIntegrationRule(rule_vol);
+  pipeline_mng->getOpBoundaryRhsPipeline().push_back(
+      new OpSetContravariantPiolaTransformOnEdge2D());
+  pipeline_mng->getOpBoundaryRhsPipeline().push_back(new BoundaryRhs("S"));
+
+  auto dm = simpleInterface->getDM();
+  F = smartCreateDMVector(dm);
+  pipeline_mng->getBoundaryRhsFE()->ksp_f = F;
+  CHKERR pipeline_mng->loopFiniteElements();
+  CHKERR VecAssemblyBegin(F);
+  CHKERR VecAssemblyEnd(F);
+  CHKERR VecGhostUpdateBegin(F, ADD_VALUES, SCATTER_REVERSE);
+  CHKERR VecGhostUpdateEnd(F, ADD_VALUES, SCATTER_REVERSE);
+
+  double fnorm;
+  CHKERR VecNorm(F, NORM_2, &fnorm);
+  MOFEM_LOG("WORLD", Sev::inform) << "Flux F norm " << std::scientific << fnorm;
+
   MoFEMFunctionReturn(0);
 }
 //! [Push operators to pipeline]
@@ -375,19 +513,52 @@ MoFEMErrorCode Example::assembleSystem() {
 //! [Solve]
 MoFEMErrorCode Example::solveSystem() {
   MoFEMFunctionBegin;
-  PipelineManager *pipeline_mng = mField.getInterface<PipelineManager>();
-  auto solver = pipeline_mng->createKSP();
-  CHKERR KSPSetFromOptions(solver);
-  CHKERR KSPSetUp(solver);
+
+  CHKERR assembleSystemIntensity();
 
   auto dm = simpleInterface->getDM();
-  auto D = smartCreateDMVector(dm);
-  auto F = smartVectorDuplicate(D);
+  PipelineManager *pipeline_mng = mField.getInterface<PipelineManager>();
 
-  CHKERR KSPSolve(solver, F, D);
-  CHKERR VecGhostUpdateBegin(D, INSERT_VALUES, SCATTER_FORWARD);
-  CHKERR VecGhostUpdateEnd(D, INSERT_VALUES, SCATTER_FORWARD);
-  CHKERR DMoFEMMeshToLocalVector(dm, D, INSERT_VALUES, SCATTER_REVERSE);
+  auto solver = MoFEM::createKSP(mField.get_comm());
+  CHKERR KSPSetFromOptions(solver);
+  CHKERR KSPSetOperators(solver, B, B);
+  CHKERR KSPSetUp(solver);
+
+  auto iD = smartCreateDMVector(dm);
+  CHKERR KSPSolve(solver, F, iD);
+  CHKERR VecGhostUpdateBegin(iD, INSERT_VALUES, SCATTER_FORWARD);
+  CHKERR VecGhostUpdateEnd(iD, INSERT_VALUES, SCATTER_FORWARD);
+
+  CHKERR DMoFEMMeshToLocalVector(dm, iD, INSERT_VALUES, SCATTER_REVERSE);
+  double i_lambda_flux;
+  CHKERR calculateFlux(i_lambda_flux);
+  MOFEM_LOG_CHANNEL("WORLD");
+  MOFEM_LOG("WORLD", Sev::inform)
+      << "iD flux " << std::scientific << i_lambda_flux;
+
+  auto lambdaD = smartVectorDuplicate(iD);
+  CHKERR assembleSystemFlux();
+  CHKERR KSPSolve(solver, F, lambdaD);
+  CHKERR VecGhostUpdateBegin(lambdaD, INSERT_VALUES, SCATTER_FORWARD);
+  CHKERR VecGhostUpdateEnd(lambdaD, INSERT_VALUES, SCATTER_FORWARD);
+
+  CHKERR DMoFEMMeshToLocalVector(dm, lambdaD, INSERT_VALUES, SCATTER_REVERSE);
+  double lambda_flux;
+  CHKERR calculateFlux(lambda_flux);
+  MOFEM_LOG_CHANNEL("WORLD");
+  MOFEM_LOG("WORLD", Sev::inform)
+      << "lambdaD flux " << std::scientific << lambda_flux;
+
+  double lambda = -i_lambda_flux / lambda_flux;
+
+  // CHKERR VecAXPY(iD, lambda, lambdaD);
+  // CHKERR DMoFEMMeshToLocalVector(dm, iD, INSERT_VALUES, SCATTER_REVERSE);
+  // double zero_flux;
+  // CHKERR calculateFlux(zero_flux);
+  // MOFEM_LOG_CHANNEL("WORLD");
+  // MOFEM_LOG("WORLD", Sev::inform)
+  //     << "Zero flux " << std::scientific << zero_flux;
+
   MoFEMFunctionReturn(0);
 }
 //! [Solve]
@@ -398,7 +569,6 @@ MoFEMErrorCode Example::outputResults(const int i) {
   PipelineManager *pipeline_mng = mField.getInterface<PipelineManager>();
   pipeline_mng->getDomainLhsFE().reset();
   pipeline_mng->getDomainRhsFE().reset();
-  pipeline_mng->getBoundaryLhsFE().reset();
   pipeline_mng->getBoundaryRhsFE().reset();
   auto post_proc_fe = boost::make_shared<PostProcFaceOnRefinedMesh>(mField);
   post_proc_fe->generateReferenceElementMesh();
