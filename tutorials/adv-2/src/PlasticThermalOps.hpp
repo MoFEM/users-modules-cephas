bc0805486cf43c8e510c2dcbc9fff039fa92db12
Daniele <daniele.barbera@glasgow.ac.uk>
2022-05-05 13:36:46 +0100

    Thermo Elasticity

--- tutorials/adv-2/src/PlasticThermalOps.hpp
+++ tutorials/adv-2/src/PlasticThermalOps.hpp
@@ -57,6 +57,32 @@ private:
   boost::shared_ptr<CommonData> commonDataPtr;
 };
 
+struct OpKCauchyThermoElasticity : public AssemblyDomainEleOp {
+  OpKCauchyThermoElasticity(const std::string row_field_name,
+                            const std::string col_field_name,
+                            boost::shared_ptr<CommonData> common_data_ptr,
+                            boost::shared_ptr<MatrixDouble> mDptr);
+  MoFEMErrorCode iNtegrate(DataForcesAndSourcesCore::EntData &row_data,
+                           DataForcesAndSourcesCore::EntData &col_data);
+
+private:
+  boost::shared_ptr<CommonData> commonDataPtr;
+  boost::shared_ptr<MatrixDouble> mDPtr;
+};
+
+struct OpPlasticStressThermal : public DomainEleOp {
+  OpPlasticStressThermal(const std::string field_name,
+                         boost::shared_ptr<CommonData> common_data_ptr,
+                         boost::shared_ptr<MatrixDouble> mDPtr,
+                         const double scale = 1);
+  MoFEMErrorCode doWork(int side, EntityType type, EntData &data);
+
+private:
+  boost::shared_ptr<MatrixDouble> mDPtr;
+  const double scaleStress;
+  boost::shared_ptr<CommonData> commonDataPtr;
+};
+
 struct OpCalculateContrainsLhs_dT : public AssemblyDomainEleOp {
   OpCalculateContrainsLhs_dT(const std::string row_field_name,
                              const std::string col_field_name,
@@ -121,9 +147,9 @@ OpPlasticHeatProduction_dEP::OpPlasticHeatProduction_dEP(
   sYmm = false;
 }
 
-MoFEMErrorCode OpPlasticHeatProduction_dEP::iNtegrate(
-    EntitiesFieldData::EntData &row_data,
-    EntitiesFieldData::EntData &col_data) {
+MoFEMErrorCode
+OpPlasticHeatProduction_dEP::iNtegrate(EntitiesFieldData::EntData &row_data,
+                                       EntitiesFieldData::EntData &col_data) {
   MoFEMFunctionBegin;
 
   auto &locMat = AssemblyDomainEleOp::locMat;
@@ -180,6 +206,103 @@ MoFEMErrorCode OpPlasticHeatProduction_dEP::iNtegrate(
   MoFEMFunctionReturn(0);
 }
 
+OpKCauchyThermoElasticity::OpKCauchyThermoElasticity(
+    const std::string row_field_name, const std::string col_field_name,
+    boost::shared_ptr<CommonData> common_data_ptr,
+    boost::shared_ptr<MatrixDouble> mDptr)
+    : AssemblyDomainEleOp(row_field_name, col_field_name,
+                          DomainEleOp::OPROWCOL),
+      commonDataPtr(common_data_ptr), mDPtr(mDptr) {
+  sYmm = false;
+}
+
+MoFEMErrorCode OpKCauchyThermoElasticity::iNtegrate(
+    DataForcesAndSourcesCore::EntData &row_data,
+    DataForcesAndSourcesCore::EntData &col_data) {
+  MoFEMFunctionBegin;
+
+  auto &locMat = AssemblyDomainEleOp::locMat;
+
+  const size_t nb_integration_pts = row_data.getN().size1();
+  const size_t nb_row_base_functions = row_data.getN().size2();
+  auto t_w = getFTensor0IntegrationWeight();
+
+  constexpr auto t_kd = FTensor::Kronecker_Delta<int>();
+  auto t_row_diff_base = row_data.getFTensor1DiffN<SPACE_DIM>();
+  auto t_D =
+      getFTensor4DdgFromMat<SPACE_DIM, SPACE_DIM, 0>(*commonDataPtr->mDPtr);
+  FTensor::Tensor2_symmetric<double, SPACE_DIM> t_eigen_strain;
+  t_eigen_strain(i, j) = (t_D(i, j, k, l) * t_kd(k, l)) * coeff_expansion;
+
+  for (size_t gg = 0; gg != nb_integration_pts; ++gg) {
+
+    double alpha = getMeasure() * t_w;
+    size_t rr = 0;
+    for (; rr != AssemblyDomainEleOp::nbRows / SPACE_DIM; ++rr) {
+      auto t_mat = getFTensor1FromMat<SPACE_DIM, 1>(locMat, rr * SPACE_DIM);
+      auto t_col_base = col_data.getFTensor0N(gg, 0);
+      for (size_t cc = 0; cc != AssemblyDomainEleOp::nbCols; cc++) {
+
+        t_mat(i) -=
+            (t_row_diff_base(j) * t_eigen_strain(i, j)) * (t_col_base * alpha);
+
+        ++t_mat;
+        ++t_col_base;
+      }
+
+      ++t_row_diff_base;
+    }
+    for (; rr != nb_row_base_functions; ++rr)
+      ++t_row_diff_base;
+
+    ++t_w;
+  }
+
+  MoFEMFunctionReturn(0);
+}
+
+OpPlasticStressThermal::OpPlasticStressThermal(
+    const std::string field_name, boost::shared_ptr<CommonData> common_data_ptr,
+    boost::shared_ptr<MatrixDouble> m_D_ptr, const double scale)
+    : DomainEleOp(field_name, DomainEleOp::OPROW),
+      commonDataPtr(common_data_ptr), scaleStress(scale), mDPtr(m_D_ptr) {
+  // Opetor is only executed for vertices
+  std::fill(&doEntities[MBEDGE], &doEntities[MBMAXTYPE], false);
+}
+
+//! [Calculate stress]
+MoFEMErrorCode OpPlasticStressThermal::doWork(int side, EntityType type,
+                                              EntData &data) {
+  MoFEMFunctionBegin;
+  const size_t nb_gauss_pts = commonDataPtr->mStrainPtr->size2();
+  commonDataPtr->mStressPtr->resize((SPACE_DIM * (SPACE_DIM + 1)) / 2,
+                                    nb_gauss_pts);
+  constexpr auto t_kd = FTensor::Kronecker_Delta<int>();
+  auto t_D = getFTensor4DdgFromMat<SPACE_DIM, SPACE_DIM, 0>(*mDPtr);
+  auto t_strain =
+      getFTensor2SymmetricFromMat<SPACE_DIM>(*(commonDataPtr->mStrainPtr));
+  auto t_plastic_strain =
+      getFTensor2SymmetricFromMat<SPACE_DIM>(commonDataPtr->plasticStrain);
+  auto t_stress =
+      getFTensor2SymmetricFromMat<SPACE_DIM>(*(commonDataPtr->mStressPtr));
+  auto t_temp = getFTensor0FromVec(commonDataPtr->tempVal);
+  for (size_t gg = 0; gg != nb_gauss_pts; ++gg) {
+    t_stress(i, j) =
+        t_D(i, j, k, l) * (t_strain(k, l) - t_plastic_strain(k, l)) -
+        t_D(i, j, k, l) * t_kd(k, l) * (t_temp - ref_temp) * coeff_expansion;
+    t_stress(i, j) /= scaleStress;
+    ++t_strain;
+    ++t_plastic_strain;
+    ++t_stress;
+    ++t_temp;
+  }
+
+  MoFEMFunctionReturn(0);
+}
+//! [Calculate stress]
+
+//
+
 OpCalculateContrainsLhs_dT::OpCalculateContrainsLhs_dT(
     const std::string row_field_name, const std::string col_field_name,
     boost::shared_ptr<CommonData> common_data_ptr)
@@ -189,9 +312,9 @@ OpCalculateContrainsLhs_dT::OpCalculateContrainsLhs_dT(
   sYmm = false;
 }
 
-MoFEMErrorCode OpCalculateContrainsLhs_dT::iNtegrate(
-    EntitiesFieldData::EntData &row_data,
-    EntitiesFieldData::EntData &col_data) {
+MoFEMErrorCode
+OpCalculateContrainsLhs_dT::iNtegrate(EntitiesFieldData::EntData &row_data,
+                                      EntitiesFieldData::EntData &col_data) {
   MoFEMFunctionBegin;
 
   auto &locMat = AssemblyDomainEleOp::locMat;
