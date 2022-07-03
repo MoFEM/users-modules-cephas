05e8925f6892921d89dfbf335271bfcd97917774
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgowa.ac.uk>
2022-07-03 21:43:12 +0100

    refactor pos-proc on eigen - add symm mat

--- tutorials/vec-1/eigen_elastic.cpp
+++ tutorials/vec-1/eigen_elastic.cpp
@@ -125,7 +125,6 @@ MoFEMErrorCode Example::createCommonData() {
     MoFEMFunctionReturn(0);
   };
 
-  matGradPtr = boost::make_shared<MatrixDouble>();
   matStrainPtr = boost::make_shared<MatrixDouble>();
   matStressPtr = boost::make_shared<MatrixDouble>();
   matDPtr = boost::make_shared<MatrixDouble>();
@@ -398,11 +397,11 @@ MoFEMErrorCode Example::solveSystem() {
   ePS = create_eps(mField.get_comm());
   CHKERR EPSSetOperators(ePS, K, M);
 
-  // Setup eps 
+  // Setup eps
   CHKERR setup_eps();
 
   // Deflate vectors
-  CHKERR deflate_vectors(); 
+  CHKERR deflate_vectors();
 
   // Solve problem
   CHKERR EPSSolve(ePS);
@@ -434,19 +433,40 @@ MoFEMErrorCode Example::outputResults() {
   post_proc_fe->getOpPtrVector().push_back(
       new OpSetHOInvJacToScalarBases<SPACE_DIM>(H1, inv_jac_ptr));
 
+  auto u_ptr = boost::make_shared<MatrixDouble>();
+  auto grad_ptr = boost::make_shared<MatrixDouble>();
+  auto strain_ptr = boost::make_shared<MatrixDouble>();
+  auto stress_ptr = boost::make_shared<MatrixDouble>();
+
   post_proc_fe->getOpPtrVector().push_back(
-      new OpCalculateVectorFieldGradient<SPACE_DIM, SPACE_DIM>("U",
-                                                               matGradPtr));
+      new OpCalculateVectorFieldValues<SPACE_DIM>("U", u_ptr));
   post_proc_fe->getOpPtrVector().push_back(
-      new OpSymmetrizeTensor<SPACE_DIM>("U", matGradPtr, matStrainPtr));
+      new OpCalculateVectorFieldGradient<SPACE_DIM, SPACE_DIM>("U", grad_ptr));
+  post_proc_fe->getOpPtrVector().push_back(
+      new OpSymmetrizeTensor<SPACE_DIM>("U", grad_ptr, strain_ptr));
   post_proc_fe->getOpPtrVector().push_back(
       new OpTensorTimesSymmetricTensor<SPACE_DIM, SPACE_DIM>(
-          "U", matStrainPtr, matStressPtr, matDPtr));
-  post_proc_fe->getOpPtrVector().push_back(new OpPostProcElastic<SPACE_DIM>(
-      "U", post_proc_fe->postProcMesh, post_proc_fe->mapGaussPts, matStrainPtr,
-      matStressPtr));
+          "U", strain_ptr, stress_ptr, matDPtr));
 
-  post_proc_fe->addFieldValuesPostProc("U");
+  using OpPPMap = OpPostProcMap<SPACE_DIM, SPACE_DIM>;
+
+  post_proc_fe->getOpPtrVector().push_back(
+
+      new OpPPMap(
+          post_proc_fe->postProcMesh, post_proc_fe->mapGaussPts,
+
+          OpPPMap::DataMapVec{},
+
+          OpPPMap::DataMapMat{{"U", u_ptr}},
+
+          OpPPMap::DataMapMat{},
+
+          OpPPMap::DataMapMat{{"STRAIN", strain_ptr}, {"STRESS", stress_ptr}}
+
+          )
+
+  );
+
   pipeline_mng->getDomainRhsFE() = post_proc_fe;
 
   auto dm = simple->getDM();
@@ -462,7 +482,8 @@ MoFEMErrorCode Example::outputResults() {
     CHKERR VecNorm(D, NORM_2, &nrm2r);
     MOFEM_LOG_C("EXAMPLE", Sev::inform,
                 " ncov = %d omega2 = %.8g omega = %.8g frequency  = %.8g", nn,
-                eigr, std::sqrt(std::abs(eigr)), std::sqrt(std::abs(eigr)) / (2 * M_PI));
+                eigr, std::sqrt(std::abs(eigr)),
+                std::sqrt(std::abs(eigr)) / (2 * M_PI));
     CHKERR DMoFEMMeshToLocalVector(dm, D, INSERT_VALUES, SCATTER_REVERSE);
     CHKERR pipeline_mng->loopFiniteElements();
     post_proc_fe->writeFile("out_eig_" + boost::lexical_cast<std::string>(nn) +
