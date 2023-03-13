0a3af5169c557baa4c5330c2b441849956e526ed
Andrei Shvarts <andrei.shvarts@glasgow.ac.uk>
2023-03-13 00:21:14 +0000

    add skin post proc

--- basic_finite_elements/nonlinear_elastic_materials/src/NonlinearElasticElementInterface.hpp
+++ basic_finite_elements/nonlinear_elastic_materials/src/NonlinearElasticElementInterface.hpp
@@ -20,10 +20,15 @@ struct NonlinearElasticElementInterface : public GenericElementInterface {
   
   PetscInt oRder;
   bool isDisplacementField;
+  PetscBool postProcSkin;
+  PetscBool postProcVolume;
+  PetscBool calcEnergy;
+  Range skinEnts;
   BitRefLevel bIt;
   boost::shared_ptr<NonlinearElasticElement> elasticElementPtr;
   boost::shared_ptr<ElasticMaterials> elasticMaterialsPtr;
   boost::shared_ptr<PostProcVolumeOnRefinedMesh> postProcMeshPtr;
+  boost::shared_ptr<PostProcFaceOnRefinedMesh> postProcSkinPtr;
 
   string positionField;
   string meshNodeField;
@@ -46,7 +51,16 @@ struct NonlinearElasticElementInterface : public GenericElementInterface {
     MoFEMFunctionBegin;
     isQuasiStatic = PETSC_FALSE;
     oRder = 2;
+    postProcSkin = PETSC_TRUE;
+    postProcVolume = PETSC_FALSE;
+    calcEnergy = PETSC_TRUE;
     CHKERR PetscOptionsGetBool(PETSC_NULL, "-is_quasi_static", &isQuasiStatic,
+                               PETSC_NULL);
+    CHKERR PetscOptionsGetBool(PETSC_NULL, "-post_proc_skin", &postProcSkin,
+                               PETSC_NULL);
+    CHKERR PetscOptionsGetBool(PETSC_NULL, "-post_proc_volume", &postProcVolume,
+                               PETSC_NULL);
+    CHKERR PetscOptionsGetBool(PETSC_NULL, "-calc_energy", &calcEnergy,
                                PETSC_NULL);
     CHKERR PetscOptionsGetInt(PETSC_NULL, "-order", &oRder, PETSC_NULL);
 
@@ -72,6 +86,28 @@ struct NonlinearElasticElementInterface : public GenericElementInterface {
     MoFEMFunctionReturnHot(0);
   };
 
+  MoFEMErrorCode getEntsOnMeshSkin() {
+    MoFEMFunctionBeginHot;
+
+    Range body_ents;
+    auto meshset = mField.getInterface<Simple>()->getMeshSet();
+    CHKERR mField.get_moab().get_entities_by_type(meshset, MBTET, body_ents);
+    // cout << "body_ents for skin: " << body_ents.size() << endl;
+    Skinner skin(&mField.get_moab());
+    Range skin_ents;
+    CHKERR skin.find_skin(0, body_ents, false, skin_ents);
+    ParallelComm *pcomm =
+        ParallelComm::get_pcomm(&mField.get_moab(), MYPCOMM_INDEX);
+    if (pcomm == NULL) {
+      pcomm = new ParallelComm(&mField.get_moab(), mField.get_comm());
+    }
+    CHKERR pcomm->filter_pstatus(skin_ents,
+                                 PSTATUS_SHARED | PSTATUS_MULTISHARED,
+                                 PSTATUS_NOT, -1, &skinEnts);
+
+    MoFEMFunctionReturnHot(0);
+  };
+
   MoFEMErrorCode createElements() {
     MoFEMFunctionBeginHot;
 
@@ -88,6 +124,21 @@ struct NonlinearElasticElementInterface : public GenericElementInterface {
     CHKERR elasticElementPtr->addElement("ELASTIC", positionField,
                                          meshNodeField, false);
 
+    if (postProcSkin) {
+      CHKERR getEntsOnMeshSkin();
+
+      CHKERR mField.add_finite_element("POST_PROC_SKIN");
+      CHKERR mField.modify_finite_element_add_field_row("POST_PROC_SKIN",
+                                                        positionField);
+      CHKERR mField.modify_finite_element_add_field_col("POST_PROC_SKIN",
+                                                        positionField);
+      CHKERR mField.modify_finite_element_add_field_data("POST_PROC_SKIN",
+                                                         positionField);
+      CHKERR mField.modify_finite_element_add_field_data("POST_PROC_SKIN",
+                                                         meshNodeField);
+      CHKERR mField.add_ents_to_finite_element_by_dim(skinEnts, 2,
+                                                      "POST_PROC_SKIN");
+    }
 
     MoFEMFunctionReturnHot(0);
   };
@@ -113,9 +164,13 @@ struct NonlinearElasticElementInterface : public GenericElementInterface {
     MoFEMFunctionBeginHot;
     this->dM = dm;
     CHKERR DMMoFEMAddElement(dM, "ELASTIC");
+    if (postProcSkin)
+      CHKERR DMMoFEMAddElement(dM, "POST_PROC_SKIN");
     mField.getInterface<Simple>()->getOtherFiniteElements().push_back(
         "ELASTIC");
-        
+    mField.getInterface<Simple>()->getOtherFiniteElements().push_back(
+        "POST_PROC_SKIN");
+           
     MoFEMFunctionReturnHot(0);
   };
 
@@ -184,7 +239,7 @@ struct NonlinearElasticElementInterface : public GenericElementInterface {
     if (elasticElementPtr->setOfBlocks.empty())
       MoFEMFunctionReturnHot(0);
 
-    if (!postProcMeshPtr) {
+    if (postProcVolume && !postProcMeshPtr) {
       postProcMeshPtr = boost::make_shared<PostProcVolumeOnRefinedMesh>(mField);
 
       if (mField.check_field("MESH_NODE_POSITIONS"))
@@ -203,21 +258,44 @@ struct NonlinearElasticElementInterface : public GenericElementInterface {
       }
     }
 
-    elasticElementPtr->getLoopFeEnergy().snes_ctx = SnesMethod::CTX_SNESNONE;
-    elasticElementPtr->getLoopFeEnergy().eNergy = 0;
-    // MOFEM_LOG("WORLD", Sev::inform) << "Loop energy\n";
-    CHKERR DMoFEMLoopFiniteElements(dM, "ELASTIC",
-                                    &elasticElementPtr->getLoopFeEnergy());
+    if (postProcSkin && !postProcSkinPtr) {
+      postProcSkinPtr = boost::make_shared<PostProcFaceOnRefinedMesh>(mField);
 
-    auto E = elasticElementPtr->getLoopFeEnergy().eNergy;
-    // Print elastic energy
-    MOFEM_LOG_C("WORLD", Sev::inform, "%d Time %3.2e Elastic energy %3.2e",
-                step, elasticElementPtr->getLoopFeRhs().ts_t, E);
+      if (mField.check_field("MESH_NODE_POSITIONS"))
+        CHKERR addHOOpsFace3D("MESH_NODE_POSITIONS", *postProcSkinPtr, false,
+                              false);
 
-    CHKERR DMoFEMLoopFiniteElements(dM, "ELASTIC", postProcMeshPtr);
-    auto out_name = "out_vol_" + to_string(step) + ".h5m";
+      CHKERR postProcSkinPtr->generateReferenceElementMesh();
+      CHKERR postProcSkinPtr->addFieldValuesPostProc(positionField);
+      CHKERR postProcSkinPtr->addFieldValuesPostProc(meshNodeField);
+      CHKERR postProcSkinPtr->addFieldValuesGradientPostProcOnSkin(
+          positionField, "ELASTIC");
+    }
 
-    CHKERR postProcMeshPtr->writeFile(out_name);
+    if (calcEnergy) {
+      elasticElementPtr->getLoopFeEnergy().snes_ctx = SnesMethod::CTX_SNESNONE;
+      elasticElementPtr->getLoopFeEnergy().eNergy = 0;
+      // MOFEM_LOG("WORLD", Sev::inform) << "Loop energy\n";
+      CHKERR DMoFEMLoopFiniteElements(dM, "ELASTIC",
+                                      &elasticElementPtr->getLoopFeEnergy());
+
+      auto E = elasticElementPtr->getLoopFeEnergy().eNergy;
+      // Print elastic energy
+      MOFEM_LOG_C("WORLD", Sev::inform, "%d Time %3.2e Elastic energy %3.2e",
+                  step, elasticElementPtr->getLoopFeRhs().ts_t, E);
+    }
+
+    if (postProcVolume) {
+      CHKERR DMoFEMLoopFiniteElements(dM, "ELASTIC", postProcMeshPtr);
+      auto out_name = "out_vol_" + to_string(step) + ".h5m";
+      CHKERR postProcMeshPtr->writeFile(out_name);
+    }
+
+    if (postProcSkin) {
+      CHKERR DMoFEMLoopFiniteElements(dM, "POST_PROC_SKIN", postProcSkinPtr);
+      auto out_name = "out_skin_" + to_string(step) + ".h5m";
+      CHKERR postProcSkinPtr->writeFile(out_name);
+    }
 
     MoFEMFunctionReturn(0);
   };
