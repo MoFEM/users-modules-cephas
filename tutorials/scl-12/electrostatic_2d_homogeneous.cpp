c425fea088caebdc2057e160535c8f5fdf29b6f9
MDTanzibEhsanSanglap <2829826s@student.gla.ac.uk>
2023-05-10 18:15:58 +0000

    start electrostatic tutorial

---
+++ tutorials/scl-12/electrostatic_2d_homogeneous.cpp
@@ -0,0 +1,300 @@
+/**
+ * \file electrostatic_2d_homogeneous.cpp
+ * \example electrostatic_2d_homogeneous.cpp
+ *
+ * Solution of poisson equation. Direct implementation of User Data Operators
+ * for teaching proposes.
+ *
+ * \note In practical application we suggest use form integrators to generalise
+ * and simplify code. However, here we like to expose user to ways how to
+ * implement data operator from scratch.
+ */
+
+constexpr auto field_name = "U";
+
+#include <BasicFiniteElements.hpp>
+#include <electrostatic_2d_homogeneous.hpp>
+
+using namespace MoFEM;
+using namespace Electrostatic2DHomogeneousOperators;
+
+using PostProcFaceEle =
+    PostProcBrokenMeshInMoab<FaceElementForcesAndSourcesCore>;
+
+static char help[] = "...\n\n";
+
+struct Electrostatic2DHomogeneous {
+public:
+  Electrostatic2DHomogeneous(MoFEM::Interface &m_field);
+
+  // Declaration of the main function to run analysis
+  MoFEMErrorCode runProgram();
+
+private:
+  // Declaration of other main functions called in runProgram()
+  MoFEMErrorCode readMesh();
+  MoFEMErrorCode setupProblem();
+  MoFEMErrorCode boundaryCondition();
+  MoFEMErrorCode assembleSystem();
+  MoFEMErrorCode setIntegrationRules();
+  MoFEMErrorCode solveSystem();
+  MoFEMErrorCode outputResults();
+
+  // MoFEM interfaces
+  MoFEM::Interface &mField;
+  Simple *simpleInterface;
+
+  // Field name and approximation order
+  int oRder;
+};
+
+Electrostatic2DHomogeneous::Electrostatic2DHomogeneous(MoFEM::Interface &m_field)
+    : mField(m_field) {}
+
+//! [Read mesh]
+MoFEMErrorCode Electrostatic2DHomogeneous::readMesh() {
+  MoFEMFunctionBegin;
+
+  CHKERR mField.getInterface(simpleInterface);
+  CHKERR simpleInterface->getOptions();
+  CHKERR simpleInterface->loadFile();
+
+  MoFEMFunctionReturn(0);
+}
+//! [Read mesh]
+
+//! [Setup problem]
+MoFEMErrorCode Electrostatic2DHomogeneous::setupProblem() {
+  MoFEMFunctionBegin;
+
+  CHKERR simpleInterface->addDomainField(field_name, H1,
+                                         AINSWORTH_LEGENDRE_BASE, 1);
+
+  int oRder = 3;
+  CHKERR PetscOptionsGetInt(PETSC_NULL, "", "-order", &oRder, PETSC_NULL);
+  CHKERR simpleInterface->setFieldOrder(field_name, oRder);
+
+  CHKERR simpleInterface->setUp();
+
+  MoFEMFunctionReturn(0);
+}
+//! [Setup problem]
+
+//! [Boundary condition]
+MoFEMErrorCode Electrostatic2DHomogeneous::boundaryCondition() {
+  MoFEMFunctionBegin;
+
+  auto bc_mng = mField.getInterface<BcManager>();
+
+  // Remove BCs from blockset name "BOUNDARY_CONDITION";
+  CHKERR bc_mng->removeBlockDOFsOnEntities<BcScalarMeshsetType<BLOCKSET>>(
+      simpleInterface->getProblemName(), "BOUNDARY_CONDITION",
+      std::string(field_name), true);
+
+  MoFEMFunctionReturn(0);
+}
+//! [Boundary condition]
+
+//! [Assemble system]
+MoFEMErrorCode Electrostatic2DHomogeneous::assembleSystem() {
+  MoFEMFunctionBegin;
+
+  auto pipeline_mng = mField.getInterface<PipelineManager>();
+
+  constexpr int space_dim = 2;
+
+  { // Push operators to the Pipeline that is responsible for calculating LHS
+    CHKERR AddHOOps<space_dim, space_dim, space_dim>::add(
+        pipeline_mng->getOpDomainLhsPipeline(), {H1});
+    pipeline_mng->getOpDomainLhsPipeline().push_back(
+        new OpDomainLhsMatrixK(field_name, field_name));
+  }
+
+  { // Push operators to the Pipeline that is responsible for calculating RHS
+
+    auto set_values_to_bc_dofs = [&](auto &fe) {
+      auto get_bc_hook = [&]() {
+        EssentialPreProc<TemperatureCubitBcData> hook(mField, fe, {});
+        return hook;
+      };
+      fe->preProcessHook = get_bc_hook();
+    };
+
+    auto calculate_residual_from_set_values_on_bc = [&](auto &pipeline) {
+      using DomainEle =
+          PipelineManager::ElementsAndOpsByDim<space_dim>::DomainEle;
+      using DomainEleOp = DomainEle::UserDataOperator;
+      using OpInternal = FormsIntegrators<DomainEleOp>::Assembly<
+          PETSC>::LinearForm<GAUSS>::OpGradTimesTensor<1, 1, space_dim>;
+
+      auto grad_u_vals_ptr = boost::make_shared<MatrixDouble>();
+      pipeline_mng->getOpDomainRhsPipeline().push_back(
+          new OpCalculateScalarFieldGradient<space_dim>(field_name,
+                                                        grad_u_vals_ptr));
+      pipeline_mng->getOpDomainRhsPipeline().push_back(
+          new OpInternal(field_name, grad_u_vals_ptr,
+                         [](double, double, double) constexpr { return -1; }));
+    };
+
+    CHKERR AddHOOps<space_dim, space_dim, space_dim>::add(
+        pipeline_mng->getOpDomainRhsPipeline(), {H1});
+    set_values_to_bc_dofs(pipeline_mng->getDomainRhsFE());
+    calculate_residual_from_set_values_on_bc(
+        pipeline_mng->getOpDomainRhsPipeline());
+    pipeline_mng->getOpDomainRhsPipeline().push_back(
+        new OpDomainRhsVectorF(field_name));
+  }
+
+  MoFEMFunctionReturn(0);
+}
+//! [Assemble system]
+
+//! [Set integration rules]
+MoFEMErrorCode Electrostatic2DHomogeneous::setIntegrationRules() {
+  MoFEMFunctionBegin;
+
+  auto rule_lhs = [](int, int, int p) -> int { return 2 * (p - 1); };
+  auto rule_rhs = [](int, int, int p) -> int { return p; };
+
+  auto pipeline_mng = mField.getInterface<PipelineManager>();
+  CHKERR pipeline_mng->setDomainLhsIntegrationRule(rule_lhs);
+  CHKERR pipeline_mng->setDomainRhsIntegrationRule(rule_rhs);
+
+  MoFEMFunctionReturn(0);
+}
+//! [Set integration rules]
+
+//! [Solve system]
+MoFEMErrorCode Electrostatic2DHomogeneous::solveSystem() {
+  MoFEMFunctionBegin;
+
+  auto pipeline_mng = mField.getInterface<PipelineManager>();
+
+  auto ksp_solver = pipeline_mng->createKSP();
+  CHKERR KSPSetFromOptions(ksp_solver);
+  CHKERR KSPSetUp(ksp_solver);
+
+  // Create RHS and solution vectors
+  auto dm = simpleInterface->getDM();
+  auto F = smartCreateDMVector(dm);
+  auto D = smartVectorDuplicate(F);
+
+  // Solve the system
+  CHKERR KSPSolve(ksp_solver, F, D);
+
+  // Scatter result data on the mesh
+  CHKERR VecGhostUpdateBegin(D, INSERT_VALUES, SCATTER_FORWARD);
+  CHKERR VecGhostUpdateEnd(D, INSERT_VALUES, SCATTER_FORWARD);
+  CHKERR DMoFEMMeshToLocalVector(dm, D, INSERT_VALUES, SCATTER_REVERSE);
+
+  MoFEMFunctionReturn(0);
+}
+//! [Solve system]
+
+//! [Output results]
+MoFEMErrorCode Electrostatic2DHomogeneous::outputResults() {
+  MoFEMFunctionBegin;
+
+  auto pipeline_mng = mField.getInterface<PipelineManager>();
+  pipeline_mng->getDomainLhsFE().reset();
+
+  auto post_proc_fe = boost::make_shared<PostProcFaceEle>(mField);
+
+  auto det_ptr = boost::make_shared<VectorDouble>();
+  auto jac_ptr = boost::make_shared<MatrixDouble>();
+  auto inv_jac_ptr = boost::make_shared<MatrixDouble>();
+
+  constexpr auto SPACE_DIM = 2; // dimension of problem
+
+  post_proc_fe->getOpPtrVector().push_back(
+      new OpCalculateHOJac<SPACE_DIM>(jac_ptr));
+  post_proc_fe->getOpPtrVector().push_back(
+      new OpInvertMatrix<SPACE_DIM>(jac_ptr, det_ptr, inv_jac_ptr));
+  post_proc_fe->getOpPtrVector().push_back(
+      new OpSetHOInvJacToScalarBases<SPACE_DIM>(H1, inv_jac_ptr));
+
+
+  auto u_ptr = boost::make_shared<VectorDouble>();
+  auto grad_u_ptr = boost::make_shared<MatrixDouble>();
+  post_proc_fe->getOpPtrVector().push_back(
+      new OpCalculateScalarFieldValues(field_name, u_ptr));
+
+  post_proc_fe->getOpPtrVector().push_back(
+      new OpCalculateScalarFieldGradient<SPACE_DIM>(field_name, grad_u_ptr));
+
+  using OpPPMap = OpPostProcMapInMoab<SPACE_DIM, SPACE_DIM>;
+
+  post_proc_fe->getOpPtrVector().push_back(
+
+      new OpPPMap(post_proc_fe->getPostProcMesh(),
+                  post_proc_fe->getMapGaussPts(),
+
+                  OpPPMap::DataMapVec{{"U", u_ptr}},
+
+                  OpPPMap::DataMapMat{{"GRAD_U", grad_u_ptr}},
+
+                  OpPPMap::DataMapMat{},
+
+                  OpPPMap::DataMapMat{}
+
+                  )
+
+  );
+
+  pipeline_mng->getDomainRhsFE() = post_proc_fe;
+  CHKERR pipeline_mng->loopFiniteElements();
+  CHKERR post_proc_fe->writeFile("out_result.h5m");
+
+  MoFEMFunctionReturn(0);
+}
+//! [Output results]
+
+//! [Run program]
+MoFEMErrorCode Electrostatic2DHomogeneous::runProgram() {
+  MoFEMFunctionBegin;
+
+  CHKERR readMesh();
+  CHKERR setupProblem();
+  CHKERR boundaryCondition();
+  CHKERR setIntegrationRules();
+  CHKERR assembleSystem();
+  CHKERR solveSystem();
+  CHKERR outputResults();
+
+  MoFEMFunctionReturn(0);
+}
+//! [Run program]
+
+//! [Main]
+int main(int argc, char *argv[]) {
+
+  // Initialisation of MoFEM/PETSc and MOAB data structures
+  const char param_file[] = "param_file.petsc";
+  MoFEM::Core::Initialize(&argc, &argv, param_file, help);
+
+  // Error handling
+  try {
+    // Register MoFEM discrete manager in PETSc
+    DMType dm_name = "DMMOFEM";
+    CHKERR DMRegister_MoFEM(dm_name);
+
+    // Create MOAB instance
+    moab::Core mb_instance;              // mesh database
+    moab::Interface &moab = mb_instance; // mesh database interface
+
+    // Create MoFEM instance
+    MoFEM::Core core(moab);           // finite element database
+    MoFEM::Interface &m_field = core; // finite element interface
+
+    // Run the main analysis
+    Electrostatic2DHomogeneous poisson_problem(m_field);
+    CHKERR poisson_problem.runProgram();
+  }
+  CATCH_ERRORS;
+
+  // Finish work: cleaning memory, getting statistics, etc.
+  MoFEM::Core::Finalize();
+
+  return 0;
+}
+//! [Main]
