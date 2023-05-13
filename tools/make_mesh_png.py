f42690d42e736310fec83d5ed5215962b85fc907
Lukasz Kaczmarczyk <638395893e79f12e57223b9a@bots.bitbucket.org>
2023-05-13 11:15:54 +0000

    improve png from vtk

--- tools/make_mesh_png.py
+++ tools/make_mesh_png.py
@@ -9,28 +9,28 @@ import matplotlib.image as mpimg
     
 debug=True
 
-def make_png(file, d2, field, wrap): 
+def make_png(file, args): 
     my_cmap = plt.cm.get_cmap("turbo", 124)
     mesh = pv.read(file)
     
-    if wrap:
-        mesh = mesh.warp_by_vector(wrap, factor=1)
+    if args.wrap_vector:
+        mesh = mesh.warp_by_vector(args.wrap_vector, factor=1)
     
     p = pv.Plotter(notebook=False, off_screen=True)    
-    if field:
+    if args.field:
         p.add_mesh(
             mesh, 
-            scalars=field, 
+            scalars=args.field, 
             show_edges=False, 
             smooth_shading=False, cmap=my_cmap) 
     else:
        p.add_mesh(
             mesh, 
             show_edges=True, edge_color='white', color='white') 
-    if d2:
-        p.camera_position = "xy"
-    p.camera.zoom(1.2)
-    image = p.screenshot('%spng' %file[:-3])
+    if args.d2:
+        p.camera_position = args.d2
+    p.camera.zoom(args.zoom)
+    image = p.screenshot('%spng' % file[:-3])
     
 def is_not_vtk(files):
     return not files.endswith('vtk')
@@ -40,9 +40,10 @@ if __name__ == '__main__':
         description="Convert multiple vtk files to png files using the pyvista.")
     parser.add_argument(
         "files", help="list of vtk files or a regexp mask", nargs='+')
-    parser.add_argument('-d2', '--d2', dest='d2', action='store_true')
+    parser.add_argument('-d2', '--d2', dest='d2', default='')
     parser.add_argument('-f', '--field', dest='field', default='', type=str)
-    parser.add_argument('-w', '--wrap', dest='wrap', default='', type=str)
+    parser.add_argument('-wv', '--wrap_vector', dest='wrap_vector', default='', type=str)
+    parser.add_argument('--zoom', dest='zoom', default=1.2, type=str)
     args = parser.parse_args()
     
     if debug: 
@@ -53,5 +54,5 @@ if __name__ == '__main__':
     display.start()
     
     for f in args.files:
-        make_png(f, args.d2, args.field, args.wrap)
+        make_png(f, args)
 
